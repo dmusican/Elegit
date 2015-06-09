555389800845b9637ee6acb0a80022bf94367283
Kiley Maki <makik@carleton.edu>
2015-06-09 10:24:05 -0500

    Basic javafx application that takes user input and displays it back. Also looks nice

--- src/edugit/Controller.java
+++ src/edugit/Controller.java
@@ -1,4 +1,16 @@
 package edugit;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class Controller {
+
+    @FXML private Text actionTarget;
+    @FXML private TextField inputText;
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+        actionTarget.setText(inputText.getText());
+    }
 }
