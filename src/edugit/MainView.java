33da50dd7ab8f999b12aae177dbb6a69fb72e232
Kiley Maki <makik@carleton.edu>
2015-06-09 11:30:08 -0500

    Moved some files around to clean up the structure

--- src/edugit/MainView.java
+++ src/edugit/MainView.java
@@ -6,11 +6,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class MainView extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("resources/fxml/sample.fxml"));
         primaryStage.setTitle("Hello World");
 
         Scene scene = new Scene(root, 350, 275);
