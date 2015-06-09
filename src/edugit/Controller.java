edec2ec2404c9c42b924a35aa18728b055c64688
Kiley Maki <makik@carleton.edu>
2015-06-09 13:24:44 -0500

    Set the stage (no pun intended) for selecting a file using the chooser

--- src/edugit/Controller.java
+++ src/edugit/Controller.java
@@ -6,21 +6,45 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
 import java.io.File;
 
 public class Controller {
 
+    public final String defaultPath = System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"TestClone";
+
     @FXML private Text actionTarget;
     @FXML private Label repoLocation;
-    @FXML private TextField fileNameText;
+    @FXML private Label fileLocation;
     @FXML private TextField commitText;
 
-    public Controller(){
+    public Controller(){}
+
+    private File getPathFromChooser(boolean isDirectory, String title){
+        File path = new File(defaultPath);
+
+        File returnFile;
+        if(isDirectory) {
+            DirectoryChooser chooser = new DirectoryChooser();
+            chooser.setTitle(title);
+            chooser.setInitialDirectory(path.getParentFile());
+
+            returnFile = chooser.showDialog(actionTarget.getScene().getWindow());
+        }else{
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle(title);
+            fileChooser.getExtensionFilters().addAll(
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+
+            returnFile = fileChooser.showOpenDialog(actionTarget.getScene().getWindow());
+        }
+        return returnFile;
     }
 
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
-        String fileName = fileNameText.getText();
+        String fileName = fileLocation.getText();
         String commitMessage = commitText.getText();
 
         RepoModel repo = new RepoModel(SECRET_CONSTANTS.TEST_GITHUB_TOKEN, new File(this.repoLocation.getText()));
@@ -34,15 +58,18 @@ public class Controller {
     }
 
     public void handleChooseRepoLocationButton(ActionEvent actionEvent) {
+        File selectedDirectory = this.getPathFromChooser(true, "Git Repositories");
 
-        File path = new File(System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"TestClone");
-
-        DirectoryChooser chooser = new DirectoryChooser();
-        chooser.setTitle("Git Repositories");
-        chooser.setInitialDirectory(path.getParentFile());
-        File selectedDirectory = chooser.showDialog(actionTarget.getScene().getWindow());
         if(selectedDirectory != null){
             this.repoLocation.setText(selectedDirectory.toString());
+        }
+    }
+
+    public void handleChooseFileButton(ActionEvent actionEvent) {
+        File selectedFile = this.getPathFromChooser(false, "Repo Files");
+
+        if(selectedFile != null){
+            this.fileLocation.setText(selectedFile.getName());
         }
     }
 }
