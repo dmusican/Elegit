fdbea9a1c1f7c13047033db6fcafecc00bcfce6b
Kiley Maki <makik@carleton.edu>
2015-06-09 14:09:29 -0500

    Files selected now display the relative path from the repo directory. Buttons are smartly disabled/enabled

--- src/edugit/Controller.java
+++ src/edugit/Controller.java
@@ -2,6 +2,7 @@ package edugit;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
@@ -14,10 +15,17 @@ public class Controller {
 
     public final String defaultPath = System.getProperty("user.home")+File.separator+"Desktop"+File.separator+"TestClone";
 
+    private File selectedRepo;
+    private File selectedFile;
+
     @FXML private Text actionTargetText;
     @FXML private Label repoPathLabel;
     @FXML private Label fileNameLabel;
     @FXML private TextField commitText;
+
+    @FXML private Button chooseRepoButton;
+    @FXML private Button chooseFileButton;
+    @FXML private Button commitButton;
 
     public Controller(){}
 
@@ -35,12 +43,22 @@ public class Controller {
             FileChooser fileChooser = new FileChooser();
             fileChooser.setTitle(title);
             fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("Text Files", "*.txt"),
-                    new FileChooser.ExtensionFilter("All Files", "*.*"));
+                    new FileChooser.ExtensionFilter("All Files", "*.*"),
+                    new FileChooser.ExtensionFilter("Text Files", "*.txt"));
 
             returnFile = fileChooser.showOpenDialog(actionTargetText.getScene().getWindow());
         }
         return returnFile;
+    }
+
+    private String getRelativePath(File parent, File file){
+        return parent.toURI().relativize(file.toURI()).getPath();
+    }
+
+    private void updateButtonDisables() {
+        chooseRepoButton.setDisable(false);
+        chooseFileButton.setDisable(selectedRepo == null);
+        commitButton.setDisable(selectedRepo == null || selectedFile == null);
     }
 
     public void handleSubmitButtonAction(ActionEvent actionEvent) {
@@ -58,18 +76,22 @@ public class Controller {
     }
 
     public void handleChooseRepoLocationButton(ActionEvent actionEvent) {
-        File selectedDirectory = this.getPathFromChooser(true, "Git Repositories");
+        File newSelectedRepo = this.getPathFromChooser(true, "Git Repositories");
 
-        if(selectedDirectory != null){
-            this.repoPathLabel.setText(selectedDirectory.toString());
+        if(newSelectedRepo != null){
+            this.selectedRepo = newSelectedRepo;
+            this.repoPathLabel.setText(selectedRepo.toString());
+            this.updateButtonDisables();
         }
     }
 
     public void handleChooseFileButton(ActionEvent actionEvent) {
-        File selectedFile = this.getPathFromChooser(false, "Repo Files");
+        File newSelectedFile = this.getPathFromChooser(false, "Repo Files");
 
-        if(selectedFile != null){
-            this.fileNameLabel.setText(selectedFile.toString());
+        if(newSelectedFile != null){
+            this.selectedFile = newSelectedFile;
+            this.fileNameLabel.setText(getRelativePath(selectedRepo, selectedFile));
+            this.updateButtonDisables();
         }
     }
 }
