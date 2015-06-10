7d6c93fdb71dc6ea21abac8f2acabc0674db4992
Graham Earley <earleyg@carleton.edu>
2015-06-10 09:59:07 -0500

    Made SessionModel a singleton

--- src/edugit/SessionModel.java
+++ src/edugit/SessionModel.java
@@ -4,15 +4,24 @@ import java.io.File;
 import java.util.ArrayList;
 
 /**
- * The SessionModel stores all the Repos in the session and lets the user
+ * The Singleton SessionModel stores all the Repos in the session and lets the user
  * switch between repos.
  */
 public class SessionModel {
 
     RepoHelper openRepoHelper;
     ArrayList<RepoHelper> allRepoHelpers;
+    SessionModel sessionModel;
 
-    public SessionModel(RepoHelper repoHelper) {
+    public SessionModel getSessionModel(RepoHelper repoHelper) {
+        if (this.sessionModel == null) {
+            this.sessionModel = new SessionModel(repoHelper);
+        }
+
+        return this.sessionModel;
+    }
+
+    private SessionModel(RepoHelper repoHelper) {
         this.openRepoHelper = repoHelper;
         this.allRepoHelpers.add(repoHelper);
     }
