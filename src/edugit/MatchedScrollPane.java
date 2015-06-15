6209c33b5b929ec00d0078f2c1fea9687ed5f279
Kiley Maki <makik@carleton.edu>
2015-06-15 09:30:22 -0500

    Made MatchedScrollPane a lot more simple

--- src/edugit/MatchedScrollPane.java
+++ src/edugit/MatchedScrollPane.java
@@ -1,36 +1,20 @@
 package edugit;
 
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Group;
 import javafx.scene.control.ScrollPane;
-
-import java.util.ArrayList;
 
 /**
  * Created by makik on 6/11/15.
  */
 public class MatchedScrollPane extends ScrollPane{
 
-    private static double vPos = 0.0;
-
-    private static ArrayList<MatchedScrollPane> instances = new ArrayList<>(2);
+    private static final DoubleProperty vPos = new SimpleDoubleProperty(0.0);
 
     public MatchedScrollPane(Group g){
         super(g);
-
-        this.setVvalue(vPos);
-
-        instances.add(this);
-
-        vvalueProperty().addListener((observable, oldValue, newValue) -> {
-            updateVerticalPosition(newValue.doubleValue());
-        });
-    }
-
-    public static void updateVerticalPosition(double newPos){
-        vPos = newPos;
-        for(MatchedScrollPane sp : instances){
-            sp.setVvalue(vPos);
-        }
+        this.vvalueProperty().bindBidirectional(vPos);
     }
 
     public void requestFocus(){}
