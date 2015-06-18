accaffa0fae3ced6da7bd761f17d9df5cee65c1c
Kiley Maki <makik@carleton.edu>
2015-06-18 11:16:33 -0500

    The commit trees now look good and avoid weird overlap issues. Commits are placed vertically based on time and packed horizontally as best as possible

--- src/edugit/treefx/TreeLayout.java
+++ src/edugit/treefx/TreeLayout.java
@@ -14,9 +14,8 @@ public class TreeLayout{
     public static int V_PAD = 25;
     public static int H_PAD = 10;
 
-    private static int rootHeight;
-    private static int[] heightCounts;
     private static List<String> visited;
+    private static List<Integer> minRowUsedInColumn;
 
     private static List<Cell> allCellsSortedByTime;
 
@@ -27,69 +26,95 @@ public class TreeLayout{
     public static void doTreeLayout(TreeGraph g){
 
         TreeGraphModel treeGraphModel = g.getTreeGraphModel();
-        Cell rootCell = treeGraphModel.getRoot();
 
         allCellsSortedByTime = treeGraphModel.allCells;
         allCellsSortedByTime.sort((c1, c2) -> Long.compare(c2.getTime(), c1.getTime()));
 
-        relocateCell(rootCell);
+        relocateCells();
     }
 
     /**
-     * Records the root cell's height and initializes variables
-     * necessary for recursion, then starts relocation of every
-     * cell recursively
-     * @param root the root of the tree upon which the tree is built
+     * Places all cells in the graph column by column, starting at the
+     * cell furthest from the top (the oldest cell). Tracks the row at
+     * which each column placed ends so as to make sure the space used
+     * is as compact as possible.
      */
-    private static void relocateCell(Cell root){
-        rootHeight = getHeightOfCell(root);
-        heightCounts = new int[allCellsSortedByTime.size()+1];
+    private static void relocateCells(){
         visited = new ArrayList<>();
+        minRowUsedInColumn = new ArrayList<>();
 
-        relocateCell(root, rootHeight);
+        int currentColumn;
+        for(int i = allCellsSortedByTime.size() - 1; i >= 0; i--){
+            Cell c = allCellsSortedByTime.get(i);
+            if(!visited.contains(c.getCellId())){
+                currentColumn = getColumnOfCell(c);
+                int minHeight = relocateCellColumn(c);
+                if(minRowUsedInColumn.size()-1 < currentColumn){
+                    minRowUsedInColumn.add(currentColumn, minHeight);
+                }else if(minHeight < minRowUsedInColumn.get(currentColumn)){
+                    minRowUsedInColumn.set(currentColumn, minHeight);
+                }
+            }
+        }
     }
 
     /**
-     * Moves the given cell to its appropriate x and y position based on its
-     * depth in the tree and how many cells have already been placed on that
-     * depth. Gets called recursively on the children of the given cell by
-     * order of their respective heights
-     * @param c the cell to relocate
-     * @param lastHeight the height of the previously added cell
+     * Places the given cell into the column closest to the
+     * left of the screen, and then chooses its child closest
+     * to the top to place in next. Each recursive call happens
+     * on only a single of c's children to allow the columns to
+     * space correctly
+     * @param c the cell to place
+     * @return the minimum row in which a cell was placed before
+     * it had no non-visited children
      */
-    private static void relocateCell(Cell c, int lastHeight){
+    private static int relocateCellColumn(Cell c){
         visited.add(c.getCellId());
 
-        int h = getHeightOfCell(c);
+        int w = getColumnOfCell(c);
+        int h = getRowOfCell(c);
 
-        c.xLocationProperty.set(heightCounts[h]);
+        c.xLocationProperty.set(w);
         c.yLocationProperty.set(h);
 
         double x = c.xLocationProperty.get() * H_SPACING + H_PAD;
         double y = c.yLocationProperty.get() * V_SPACING + V_PAD;
         c.relocate(x, y);
 
-        heightCounts[h] += 1;
-
-        for(int i = h; i < lastHeight; i++){
-            if(heightCounts[i] < heightCounts[lastHeight]){
-                heightCounts[i] += 1;
-            }
-        }
-
         List<Cell> list = c.getCellChildren();
         list.sort((c1, c2) -> Long.compare(c2.getTime(), c1.getTime()));
 
         for(Cell child : list){
             if(!visited.contains(child.getCellId())){
-                relocateCell(child, h);
+                return relocateCellColumn(child);
             }
         }
+        return h;
     }
 
-    private static int getHeightOfCell(Cell c){
-//        return c.height;
+    /**
+     * Calculates the column closest to the left of the screen to place the
+     * given cell based on the cell's row and the minimum heights recorded
+     * for each column
+     * @param c the cell to examine
+     * @return the lowest indexed column in which to place c
+     */
+    private static int getColumnOfCell(Cell c){
+        int column = 0;
+        int cellRow = getRowOfCell(c);
+        while(minRowUsedInColumn.size() > column && cellRow > minRowUsedInColumn.get(column)){
+            column++;
+        }
+        return column;
+    }
 
+    /**
+     * Gets the row of the given cell, with row 0 being the top of the screen
+     * and the root cell of the tree being at the bottom
+     * @param c the cell to examine
+     * @return the row index of this cell
+     */
+    private static int getRowOfCell(Cell c){
         return allCellsSortedByTime.indexOf(c);
     }
 }
