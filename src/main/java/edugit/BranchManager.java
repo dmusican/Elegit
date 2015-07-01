ffd83995fe4448ffef58634cbef495461ed186d6
Graham Earley <earleyg@carleton.edu>
2015-07-01 15:38:05 -0500

    Added initial branch merge support! Also allowed multiple branch deletion at a time and did some refactoring.

--- src/main/java/edugit/BranchManager.java
+++ src/main/java/edugit/BranchManager.java
@@ -1,37 +1,25 @@
 package main.java.edugit;
 
 import javafx.collections.FXCollections;
-import javafx.collections.ListChangeListener;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import org.controlsfx.control.CheckListView;
-import org.controlsfx.control.ListSelectionView;
 import org.controlsfx.control.NotificationPane;
 import org.controlsfx.control.action.Action;
 import org.eclipse.jgit.api.CreateBranchCommand;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.MergeResult;
 import org.eclipse.jgit.api.errors.*;
-import org.eclipse.jgit.lib.BranchTrackingStatus;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.transport.RefSpec;
 
 import java.io.IOException;
-import java.rmi.Remote;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -51,6 +39,10 @@ public class BranchManager {
 
     private TextField newBranchNameField;
 
+    private Button mergeButton;
+    private Button deleteLocalBranchesButton;
+    private Button trackRemoteBranchButton;
+
     public BranchManager(ArrayList<LocalBranchHelper> localBranches, ArrayList<RemoteBranchHelper> remoteBranches, Repository repo) throws IOException {
         this.repo = repo;
 
@@ -58,33 +50,29 @@ public class BranchManager {
         this.localListView = new ListView<>(FXCollections.observableArrayList(localBranches));
 
         this.remoteListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-        this.localListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        this.remoteListView.setOnMouseClicked(e -> {
+            try {
+                this.updateButtons();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        // Local list view can select multiple (for merges):
+        this.localListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+        this.localListView.setOnMouseClicked(e -> {
+            try {
+                this.updateButtons();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
         this.newBranchNameField = new TextField();
         this.newBranchNameField.setPromptText("Branch name");
-    }
 
-    /**
-     * Creates and populate the window that lets the user
-     * create, delete, or track branches.
-     *
-     * @throws IOException
-     */
-    public void showBranchChooserWindow() throws IOException {
-        int PADDING = 10;
-        GridPane root = new GridPane();
-        root.setHgap(PADDING);
-        root.setVgap(PADDING);
-        root.setPadding(new Insets(PADDING));
-
-
-        root.add(new Text("Remote branches:"), 0, 0); // col, row
-        root.add(new Text("Local branches:"), 1, 0);
-        root.add(this.remoteListView, 0, 1);
-        root.add(this.localListView, 1, 1);
-
-        Button trackRemoteBranchButton = new Button("Track branch locally");
-        trackRemoteBranchButton.setOnAction(e -> {
+        this.trackRemoteBranchButton = new Button("Track branch locally");
+        this.trackRemoteBranchButton.setOnAction(e -> {
             try {
                 this.trackSelectedBranchLocally();
             } catch (GitAPIException e1) {
@@ -93,18 +81,56 @@ public class BranchManager {
                 e1.printStackTrace();
             }
         });
-        Button deleteLocalBranchButton = new Button("Delete local branch");
-        deleteLocalBranchButton.setOnAction(e -> this.deleteSelectedLocalBranch());
+        this.trackRemoteBranchButton.setDisable(true);
 
-        HBox hButtons = new HBox(trackRemoteBranchButton, deleteLocalBranchButton);
+        this.deleteLocalBranchesButton = new Button("Delete local branch");
+        this.deleteLocalBranchesButton.setOnAction(e -> this.deleteSelectedLocalBranches());
+        this.deleteLocalBranchesButton.setDisable(true);
+
+        this.mergeButton = new Button();
+        this.mergeButton.setOnAction(e -> {
+            try {
+                this.mergeSelectedBranchWithCurrent();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            } catch (GitAPIException e1) {
+                e1.printStackTrace();
+            }
+        });
+        this.updateButtons();
+    }
+
+    /**
+     * Creates and populate the window that lets the user
+     * create, delete, or track branches.
+     *
+     * A GridPane is set up programmatically in this function.
+     *
+     * @throws IOException
+     */
+    public void showBranchChooserWindow() throws IOException {
+        final int PADDING = 10;
+        GridPane root = new GridPane();
+        root.setHgap(PADDING);
+        root.setVgap(PADDING);
+        root.setPadding(new Insets(PADDING));
+        root.setAlignment(Pos.TOP_CENTER);
+
+        root.add(new Text("Remote branches:"), 0, 0); // col, row
+        root.add(new Text("Local branches:"), 1, 0);
+        root.add(this.remoteListView, 0, 1);
+        root.add(this.localListView, 1, 1);
+
+        this.updateButtons();
+
+        HBox hButtons = new HBox(trackRemoteBranchButton, this.mergeButton, deleteLocalBranchesButton);
         hButtons.setAlignment(Pos.CENTER);
         hButtons.setSpacing(PADDING);
-        hButtons.setPrefWidth(this.localListView.getPrefWidth()+PADDING+this.remoteListView.getPrefWidth());
         root.add(hButtons, 0, 2, 2, 1);
 
-        root.add(new Text(String.format("Branch off from %s:", this.repo.getBranch())), 0, 3, 2, 1); // colspan = 2
+        root.add(new Text(String.format("Branch off from %s:", this.repo.getBranch())), 0, 4, 2, 1); // colspan = 2
 
-        root.add(this.newBranchNameField, 0, 4);
+        root.add(this.newBranchNameField, 0, 5);
 
         Button newBranchButton = new Button("Create branch");
         newBranchButton.setOnAction(e -> {
@@ -115,21 +141,51 @@ public class BranchManager {
                 this.showInvalidBranchNameNotification();
                 e1.printStackTrace();
             } catch (GitAPIException e1) {
-                this.showGenericGitError();
+                this.showGenericGitErrorNotification();
                 e1.printStackTrace();
             } catch (IOException e1) {
-                this.showGenericError();
+                this.showGenericErrorNotification();
                 e1.printStackTrace();
             }
         });
-        root.add(newBranchButton, 1, 4);
+        root.add(newBranchButton, 1, 5);
 
         Stage stage = new Stage();
         stage.setTitle("Branch Manager");
         this.notificationPane = new NotificationPane(root);
         this.notificationPane.getStylesheets().add("/main/resources/edugit/css/BaseStyle.css");
-        stage.setScene(new Scene(this.notificationPane, 450, 450));
+        stage.setScene(new Scene(this.notificationPane, 550, 450));
         stage.show();
+    }
+
+    /**
+     * Updates the track remote, merge, and delete local buttons'
+     * text and/or disabled/enabled status.
+     *
+     * @throws IOException
+     */
+    private void updateButtons() throws IOException {
+        String currentBranchName = this.repo.getBranch();
+
+        // Update delete button
+        if (this.localListView.getSelectionModel().getSelectedIndices().size() > 0) {
+            this.deleteLocalBranchesButton.setDisable(false);
+        }
+
+        // Update track button
+        if (this.remoteListView.getSelectionModel().getSelectedIndices().size() > 0) {
+            this.trackRemoteBranchButton.setDisable(false);
+        }
+
+        // Update merge button
+        if (this.localListView.getSelectionModel().getSelectedIndices().size() == 1) {
+            this.mergeButton.setDisable(false);
+            String selectedBranchName = this.localListView.getSelectionModel().getSelectedItem().getBranchName();
+            this.mergeButton.setText(String.format("Merge %s into %s", selectedBranchName, currentBranchName));
+        } else {
+            this.mergeButton.setText(String.format("Merge selected branch into %s", currentBranchName));
+            this.mergeButton.setDisable(true);
+        }
     }
 
     /**
@@ -173,27 +229,28 @@ public class BranchManager {
     }
 
     /**
-     * Deletes the selected branch (in the localListView) through git.
+     * Deletes the selected branches (in the localListView) through git.
      */
-    public void deleteSelectedLocalBranch() {
-        LocalBranchHelper selectedBranch = this.localListView.getSelectionModel().getSelectedItem();
-        Git git = new Git(this.repo);
+    public void deleteSelectedLocalBranches() {
+        for (LocalBranchHelper selectedBranch : this.localListView.getSelectionModel().getSelectedItems()) {
+            Git git = new Git(this.repo);
 
-        try {
-            if (selectedBranch != null) {
-                // Local delete:
-                git.branchDelete().setBranchNames(selectedBranch.getRefPathString()).call();
-                this.localListView.getItems().remove(selectedBranch);
+            try {
+                if (selectedBranch != null) {
+                    // Local delete:
+                    git.branchDelete().setBranchNames(selectedBranch.getRefPathString()).call();
+                    this.localListView.getItems().remove(selectedBranch);
+                }
+            } catch (NotMergedException e) {
+                this.showNotMergedNotification(selectedBranch);
+                e.printStackTrace();
+            } catch (CannotDeleteCurrentBranchException e) {
+                this.showCannotDeleteBranchNotification();
+                e.printStackTrace();
+            } catch (GitAPIException e) {
+                this.showGenericGitErrorNotification();
+                e.printStackTrace();
             }
-        } catch (NotMergedException e) {
-            this.showNotMergedNotification();
-            e.printStackTrace();
-        } catch (CannotDeleteCurrentBranchException e) {
-            this.showCannotDeleteBranchNotification();
-            e.printStackTrace();
-        } catch (GitAPIException e) {
-            this.showGenericGitError();
-            e.printStackTrace();
         }
 
         // TODO: add optional delete from remote, too.
@@ -217,51 +274,108 @@ public class BranchManager {
     }
 
     /**
-     * Deletes the selected branch through git, forcefully.
+     * Deletes a given local branch through git, forcefully.
      */
-    private void forceDeleteSelectedLocalBranch() {
-        LocalBranchHelper selectedBranch = this.localListView.getSelectionModel().getSelectedItem();
+    private void forceDeleteLocalBranch(LocalBranchHelper branchToDelete) {
         Git git = new Git(this.repo);
 
         try {
-            if (selectedBranch != null) {
+            if (branchToDelete != null) {
                 // Local delete:
-                git.branchDelete().setForce(true).setBranchNames(selectedBranch.getRefPathString()).call();
-                this.localListView.getItems().remove(selectedBranch);
+                git.branchDelete().setForce(true).setBranchNames(branchToDelete.getRefPathString()).call();
+                this.localListView.getItems().remove(branchToDelete);
             }
-        } catch (NotMergedException e) {
-            this.showNotMergedNotification();
-            e.printStackTrace();
         } catch (CannotDeleteCurrentBranchException e) {
             this.showCannotDeleteBranchNotification();
             e.printStackTrace();
         } catch (GitAPIException e) {
-            this.showGenericGitError();
+            this.showGenericGitErrorNotification();
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Performs a git merge between the currently checked out branch and
+     * the selected local branch.
+     *
+     * @throws IOException
+     * @throws GitAPIException
+     */
+    public void mergeSelectedBranchWithCurrent() throws IOException, GitAPIException {
+        Git git = new Git(this.repo);
+        LocalBranchHelper selectedBranch = this.localListView.getSelectionModel().getSelectedItem();
+
+        MergeCommand merge = git.merge();
+        merge.include(this.repo.resolve(selectedBranch.getRefPathString()));
+
+        MergeResult mergeResult = merge.call();
+
+        if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.CONFLICTING)){
+            this.showConflictsNotification();
+        } else if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.ALREADY_UP_TO_DATE)) {
+            this.showUpToDateNotification();
+        } else if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.FAILED)) {
+            this.showFailedMergeNotification();
+        } else if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.MERGED)
+                || mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.MERGED_NOT_COMMITTED)) {
+            this.showMergeSuccessNotification();
+        } else if (mergeResult.getMergeStatus().equals(MergeResult.MergeStatus.FAST_FORWARD)) {
+            this.showFastForwardMergeNotification();
+        } else {
+            System.out.println(mergeResult.getMergeStatus());
+            // todo: handle all cases (maybe combine some)
         }
     }
 
     /// BEGIN: ERROR NOTIFICATIONS:
 
-    private void showGenericGitError() {
+    private void showFastForwardMergeNotification() {
+        this.notificationPane.setText("Fast-forward merge completed (HEAD was updated).");
+
+        this.notificationPane.getActions().clear();
+        this.notificationPane.show();
+    }
+
+    private void showMergeSuccessNotification() {
+        this.notificationPane.setText("Merge completed.");
+
+        this.notificationPane.getActions().clear();
+        this.notificationPane.show();
+    }
+
+    private void showFailedMergeNotification() {
+        this.notificationPane.setText("The merge failed.");
+
+        this.notificationPane.getActions().clear();
+        this.notificationPane.show();
+    }
+
+    private void showUpToDateNotification() {
+        this.notificationPane.setText("No merge necessary. Those two branches are already up-to-date.");
+
+        this.notificationPane.getActions().clear();
+        this.notificationPane.show();
+    }
+
+    private void showGenericGitErrorNotification() {
         this.notificationPane.setText("Sorry, there was a git error.");
 
         this.notificationPane.getActions().clear();
         this.notificationPane.show();
     }
 
-    private void showGenericError() {
+    private void showGenericErrorNotification() {
         this.notificationPane.setText("Sorry, there was an error.");
 
         this.notificationPane.getActions().clear();
         this.notificationPane.show();
     }
 
-    private void showNotMergedNotification() {
+    private void showNotMergedNotification(LocalBranchHelper nonmergedBranch) {
         this.notificationPane.setText("That branch has to be merged before you can do that.");
 
         Action forceDeleteAction = new Action("Force delete", e -> {
-            this.forceDeleteSelectedLocalBranch();
+            this.forceDeleteLocalBranch(nonmergedBranch);
             this.notificationPane.hide();
         });
 
@@ -287,6 +401,13 @@ public class BranchManager {
 
     private void showInvalidBranchNameNotification() {
         this.notificationPane.setText("That branch name is invalid.");
+
+        this.notificationPane.getActions().clear();
+        this.notificationPane.show();
+    }
+
+    private void showConflictsNotification() {
+        this.notificationPane.setText("That merge resulted in conflicts. Check the working tree to resolve them.");
 
         this.notificationPane.getActions().clear();
         this.notificationPane.show();
