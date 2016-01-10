c10200f0cb92f07500b09abae41fae6a4d58d1cf
Graham Earley <graham.p.earley@gmail.com>
2016-01-10 13:17:37 -0600

    Wrote initial (failing) test for branch manager window.

--- src/main/java/elegit/tests/BranchManagerTest.java
+++ src/main/java/elegit/tests/BranchManagerTest.java
@@ -1,7 +1,16 @@
 package main.java.elegit.tests;
 
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import org.junit.Test;
 import org.loadui.testfx.GuiTest;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by grahamearley on 1/10/16.
@@ -10,6 +19,43 @@ public class BranchManagerTest extends GuiTest {
 
     @Override
     protected Parent getRootNode() {
-        return null;
+        Parent root = null;
+
+        try {
+            root = FXMLLoader.load(getClass().getResource("/elegit/fxml/MainView.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return root;
+    }
+
+    // The window size is too big for the Manage Branches button to be clicked!
+    public void setCustomStage() {
+        Platform.runLater(() -> {
+            stage.close();
+
+            Parent root = getRootNode();
+            stage.setTitle("Elegit");
+
+            Scene scene = new Scene(root, 1200, 650); // width, height
+            stage.setScene(scene);
+            stage.show();
+            sleep(3, TimeUnit.SECONDS);
+        });
+    }
+
+    @Test
+    public void testBranchManagerButton() {
+        Button managerButton = find("Manage branches");
+        click(managerButton);
+
+        sleep(5, TimeUnit.SECONDS);
+
+        click(managerButton);
+
+        Stage branchManagerWindow = findStageByTitle("Branch Manager");
+        assert branchManagerWindow.isShowing();
+
     }
 }
