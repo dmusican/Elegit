688061d7ee808950e5f76c3dda2f0860840c7eac
Graham Earley <graham.p.earley@gmail.com>
2016-01-10 10:01:41 -0600

    Created initial dummy test

---
+++ src/main/java/elegit/tests/BranchManagerTest.java
@@ -0,0 +1,15 @@
+package main.java.elegit.tests;
+
+import javafx.scene.Parent;
+import org.loadui.testfx.GuiTest;
+
+/**
+ * Created by grahamearley on 1/10/16.
+ */
+public class BranchManagerTest extends GuiTest {
+
+    @Override
+    protected Parent getRootNode() {
+        return null;
+    }
+}
