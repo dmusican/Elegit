cca14bf96ae304d3988392f94e1d31f2bf33fc42
Eric Walker <erictwalker18@gmail.com>
2016-01-13 09:32:22 -0600

    Added the backbone for logging and a class to submit logging data, includes new dependencies in maven for these applications

---
+++ src/main/java/elegit/DataSubmitter.java
@@ -0,0 +1,54 @@
+package main.java.elegit;
+
+/**
+ * Created by Eric on 1/13/2016.
+ */
+
+import java.io.*;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class DataSubmitter {
+    private static final String submitUrl = "http://localhost:8080"; //for testing, keeping the local one
+    private static final String filepath = "/filename.log";
+    public DataSubmitter() {
+
+    }
+
+    public void submitData() {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpPost httppost = new HttpPost(submitUrl);
+
+            File file = new File(filepath);
+
+            InputStreamEntity reqEntity = new InputStreamEntity(
+                    new FileInputStream(file), -1, ContentType.APPLICATION_OCTET_STREAM);
+            reqEntity.setChunked(true);
+
+            httppost.setEntity(reqEntity);
+
+            //System.out.println("Executing request: " + httppost.getRequestLine());
+            CloseableHttpResponse response = httpclient.execute(httppost);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                System.out.println(EntityUtils.toString(response.getEntity()));
+            } catch (Exception e) {
+                response.close();
+            }
+        } catch (Exception e) {
+            try {
+                httpclient.close();
+            } catch (Exception f) {
+
+            }
+        }
+    }
+}
--- src/main/java/elegit/Main.java
+++ src/main/java/elegit/Main.java
@@ -6,13 +6,48 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URI;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
+
 /**
  * The starting point for this JavaFX application.
  */
 public class Main extends Application {
+    static final Logger logger = LogManager.getLogger(Main.class);
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        System.out.println("Working Directory = " +
+                System.getProperty("user.dir"));
+        File logConfigFile = new File( "src/main/resources/elegit/config/log4j2.xml" );
+        System.setProperty( "logFilename", "filename.log" );
+
+        try {
+            FileInputStream fis = new FileInputStream( logConfigFile );
+
+            XmlConfigurationFactory fc = new XmlConfigurationFactory( );
+            fc.getConfiguration(  new ConfigurationSource( fis ) );
+
+            URI configuration = logConfigFile.toURI();
+            Configurator.initialize("config", null, configuration);
+
+            org.apache.logging.log4j.core.LoggerContext ctx =
+                    (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext( true );
+            ctx.reconfigure();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        logger.error("Entering Log4j Example.");
+
         Parent root = FXMLLoader.load(getClass().getResource("/elegit/fxml/MainView.fxml"));
         primaryStage.setTitle("Elegit");
 
