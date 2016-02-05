4150b3f8230b71d42979f2f52fef0d71bde43962
Kiley Maki <makik@carleton.edu>
2016-02-04 22:27:46 -0600

    Fixed issues where a file would sometimes get displayed more than once in the working tree and where a file would not get its status properly updated

--- src/main/java/elegit/AllFilesPanelView.java
+++ src/main/java/elegit/AllFilesPanelView.java
@@ -1,14 +1,12 @@
 package main.java.elegit;
 
+import javafx.application.Platform;
 import javafx.scene.control.TreeItem;
 import org.eclipse.jgit.api.errors.GitAPIException;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * AllFilesPanelView displays all files in the current repository,
@@ -55,17 +53,14 @@ public class AllFilesPanelView extends FileStructurePanelView{
         }
 
         // Track all currently displayed files to make sure they are still valid
-        Map<TreeItem<RepoFile>, Boolean> shouldKeepItem = new HashMap<>();
-        for(Path p : itemMap.keySet()){
-            shouldKeepItem.put(itemMap.get(p), false);
-        }
+        Set<TreeItem<RepoFile>> itemsToRemove = new HashSet<>();
+        itemsToRemove.addAll(itemMap.values());
 
         // Loop through files at each depth
         for(int i = 0; i < maxDepth + 1; i++) {
             List<RepoFile> filesAtDepth = filesAtDepthMap.get(i);
             for (RepoFile repoFile : filesAtDepth) {
                 Path pathToFile = repoFile.getFilePath();
-                TreeItem<RepoFile> newItem = new TreeItem<>(repoFile, repoFile.diffButton);
 
                 // Check if there is already a record of this file
                 if (itemMap.containsKey(pathToFile)) {
@@ -73,24 +68,33 @@ public class AllFilesPanelView extends FileStructurePanelView{
 
                     if (oldItem.getValue().equals(repoFile)) {
                         // The given file is already present, no additional processing needed
-                        shouldKeepItem.put(oldItem, true);
+                        itemsToRemove.remove(oldItem);
                     } else {
                         // The file is displayed, but needs its status updated. Replace the old with the new
-                        newItem.setExpanded(oldItem.isExpanded());
-                        newItem.getChildren().setAll(oldItem.getChildren());
+                        TreeItem<RepoFile> newItem = new TreeItem<>(repoFile, repoFile.diffButton);
+
                         TreeItem<RepoFile> parent = oldItem.getParent();
-                        parent.getChildren().set(parent.getChildren().indexOf(oldItem), newItem);
-                        shouldKeepItem.put(newItem, true);
+
+                        Platform.runLater(() -> {
+                            newItem.setExpanded(oldItem.isExpanded());
+                            newItem.getChildren().setAll(oldItem.getChildren());
+                            parent.getChildren().set(parent.getChildren().indexOf(oldItem), newItem);
+                        });
+
+                        itemsToRemove.remove(oldItem);
                         itemMap.put(pathToFile, newItem);
                     }
                 } else {
                     // The given file wasn't present, so need to add it
+                    TreeItem<RepoFile> newItem = new TreeItem<>(repoFile, repoFile.diffButton);
+
                     Path pathToParent = pathToFile.getParent();
                     boolean foundParent = false;
                     // Make sure this new item is properly inserted as a child to its parent
                     while (pathToParent != null && !root.getValue().getFilePath().equals(pathToParent)) {
                         if (itemMap.containsKey(pathToParent)) {
-                            itemMap.get(pathToParent).getChildren().add(newItem);
+                            TreeItem<RepoFile> parent = itemMap.get(pathToParent);
+                            Platform.runLater(() -> parent.getChildren().add(newItem));
                             foundParent = true;
                             break;
                         }
@@ -98,21 +102,20 @@ public class AllFilesPanelView extends FileStructurePanelView{
                     }
                     // If no parent is found, we can assume it belongs to the root
                     if (!foundParent){
-                        root.getChildren().add(newItem);
+                        Platform.runLater(() -> root.getChildren().add(newItem));
                     }
                     itemMap.put(pathToFile, newItem);
-                    shouldKeepItem.put(newItem, true);
+                    itemsToRemove.remove(newItem);
                 }
             }
         }
 
         // Remove all elements that shouldn't be displayed
-        for (TreeItem<RepoFile> item : shouldKeepItem.keySet()) {
-            if (!shouldKeepItem.get(item)) {
-                if(item.getParent() != null)
-                    item.getParent().getChildren().remove(item);
-                itemMap.remove(item.getValue().getFilePath());
-            }
+        for (TreeItem<RepoFile> item : itemsToRemove) {
+            Platform.runLater(() -> {
+                if(item.getParent() != null) item.getParent().getChildren().remove(item);
+            });
+            itemMap.remove(item.getValue().getFilePath());
         }
     }
 
