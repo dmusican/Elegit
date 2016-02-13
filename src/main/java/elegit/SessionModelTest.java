064fd7b70455974a89c5dae473d519f1d63c51c8
Dave Musicant at home <dmusican@homemachine>
2016-02-13 05:04:58 -0600

    Added junit jar to pom.xml, and created a sample test file.

---
+++ src/main/java/elegit/SessionModelTest.java
@@ -0,0 +1,28 @@
+package main.java.elegit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * JUnit tests for SessionModel.
+ */
+public class SessionModelTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSessionModel() throws Exception {
+        assertTrue(6 == 3+3);
+    }
+}
