f580c6a642e6b0261e5fbfd7de1522e8203ac3c8
Dave on Mac Yosemite <dmusican+laptop@carleton.edu>
2016-02-24 03:59:10 -0600

    Added test for null pointer exception

---
+++ tests/main/java/elegit/ExistingRepoHelperTest.java
@@ -0,0 +1,44 @@
+package main.java.elegit;
+
+import org.eclipse.jgit.api.Git;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by dmusican on 2/24/16.
+ */
+public class ExistingRepoHelperTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
+    @Test
+    public void testExistingRepoOpen() throws Exception {
+        File localPath = File.createTempFile("TestGitRepo","");
+        localPath.delete();
+
+        Git git = Git.cloneRepository()
+                    .setURI("https://github.com/dmusican/testrepo.git")
+                    .setDirectory(localPath)
+                    .call();
+
+
+        String username = null;
+        ExistingRepoHelper repoHelper = new ExistingRepoHelper(Paths.get(localPath.getAbsolutePath()), username);
+        git.close();
+    }
+}
