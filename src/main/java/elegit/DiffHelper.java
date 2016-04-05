b99003b0a03200d5fbd530eddb969106550353e4
Kiley Maki <makik@carleton.edu>
2016-04-05 01:59:26 -0500

    Fixed an issue where the diff view of a file would show up empty on Windows

--- src/main/java/elegit/DiffHelper.java
+++ src/main/java/elegit/DiffHelper.java
@@ -5,7 +5,6 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.diff.DiffEntry;
 import org.eclipse.jgit.diff.DiffFormatter;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
@@ -16,12 +15,12 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.AbstractTreeIterator;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.eclipse.jgit.treewalk.FileTreeIterator;
+import org.eclipse.jgit.treewalk.filter.PathFilter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -50,29 +49,22 @@ public class DiffHelper {
     }
 
     private String getDiffString() throws GitAPIException, IOException {
-        ByteArrayOutputStream diffOutputStream = new ByteArrayOutputStream();
-
         ObjectId head = this.repo.resolve("HEAD");
         if(head == null) return "";
 
         // The following code is largely written by Tk421 on StackOverflow
         //      (http://stackoverflow.com/q/23486483)
         // Thanks! NOTE: comments are mine.
+        ByteArrayOutputStream diffOutputStream = new ByteArrayOutputStream();
         DiffFormatter formatter = new DiffFormatter(diffOutputStream);
         formatter.setRepository(this.repo);
+        formatter.setPathFilter(PathFilter.create(this.pathFilter.replaceAll("\\\\","/")));
 
         AbstractTreeIterator commitTreeIterator = prepareTreeParser(this.repo, head.getName());
         FileTreeIterator workTreeIterator = new FileTreeIterator(this.repo);
 
         // Scan gets difference between the two iterators.
-        List<DiffEntry> diffs = formatter.scan(commitTreeIterator, workTreeIterator);
-
-        for (DiffEntry entry : diffs) {
-            if (entry.getNewPath().equals(this.pathFilter)) {
-                // Only output the diff if it's for the file in question
-                formatter.format(entry);
-            }
-        }
+        formatter.format(commitTreeIterator, workTreeIterator);
 
         return diffOutputStream.toString();
     }
@@ -94,6 +86,7 @@ public class DiffHelper {
             } else if (line.length() > 1 && line.charAt(0) == '@' && line.charAt(1) == '@') {
                 text.setId("gitAnnotationDiffText");
             } else {
+                text.setText(" "+text.getText());
                 text.setId("unchangedDiffText");
             }
             coloredDiffList.add(text);
@@ -123,20 +116,17 @@ public class DiffHelper {
 
     private static AbstractTreeIterator prepareTreeParser(Repository repository, String objectId) throws IOException {
         // from the commit we can build the tree which allows us to construct the TreeParser
-        RevWalk walk = new RevWalk(repository);
-        RevCommit commit = walk.parseCommit(ObjectId.fromString(objectId));
-        RevTree tree = walk.parseTree(commit.getTree().getId());
+        try (RevWalk walk = new RevWalk(repository)){
+            RevCommit commit = walk.parseCommit(ObjectId.fromString(objectId));
+            RevTree tree = walk.parseTree(commit.getTree().getId());
 
-        CanonicalTreeParser oldTreeParser = new CanonicalTreeParser();
-        ObjectReader oldReader = repository.newObjectReader();
-        try {
-            oldTreeParser.reset(oldReader, tree.getId());
-        } finally {
-            oldReader.close();
+            CanonicalTreeParser oldTreeParser = new CanonicalTreeParser();
+            try (ObjectReader oldReader = repository.newObjectReader()){
+                oldTreeParser.reset(oldReader, tree.getId());
+            }
+
+            walk.dispose();
+            return oldTreeParser;
         }
-
-        walk.dispose();
-
-        return oldTreeParser;
     }
 }
