a3c36fea8a891ec78a379e1b8bd4c8de75fe788b
Eric Walker <erictwalker18@gmail.com>
2016-05-24 17:28:20 -0500

    local changes

--- src/main/java/elegit/Main.java
+++ src/main/java/elegit/Main.java
@@ -1,68 +1,86 @@
-package main.java.elegit;
+package elegit;
 
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.net.URI;
-
-import javafx.stage.WindowEvent;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.apache.logging.log4j.core.config.ConfigurationSource;
-import org.apache.logging.log4j.core.config.xml.XmlConfigurationFactory;
+
+import javax.imageio.ImageIO;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * The starting point for this JavaFX application.
  */
 public class Main extends Application {
+    private Path logPath;
+
+    public static SessionController sessionController;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        // -----------------------------------------------------------------------
-        System.setProperty("log4j.configurationFile", "src/main/resources/elegit/config/log4j2.xml");
+        // -----------------------Logging Initialization Start---------------------------
+        /* Temp directory
+        this.logPath = Files.createTempDirectory("elegitLogs");
+        this.logPath.toFile().deleteOnExit();
+        System.setProperty("logFolder", logPath.toString());
+        */
+
+        logPath = Paths.get("logs");
+        String s = logPath.toAbsolutePath().toString();
+        System.setProperty("logFolder", s);
+
         final Logger logger = LogManager.getLogger();
-
-        File logConfigFile = new File( "src/main/resources/elegit/config/log4j2.xml" );
-
-        try {
-            FileInputStream fis = new FileInputStream( logConfigFile );
-
-            XmlConfigurationFactory fc = new XmlConfigurationFactory( );
-            fc.getConfiguration(  new ConfigurationSource( fis ) );
-
-            URI configuration = logConfigFile.toURI();
-            Configurator.initialize("config", null, configuration);
-
-            org.apache.logging.log4j.core.LoggerContext ctx =
-                    (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext( true );
-            ctx.reconfigure();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        // -----------------------Logging Initialization End-----------------------------
 
         logger.info("Starting up.");
 
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/elegit/fxml/MainView.fxml"));
+        fxmlLoader.load();
+        Pane root = fxmlLoader.getRoot();
+        sessionController = fxmlLoader.getController();
 
-        Parent root = FXMLLoader.load(getClass().getResource("/elegit/fxml/MainView.fxml"));
         primaryStage.setTitle("Elegit");
-        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-            @Override
-            public void handle(WindowEvent event) {
+
+        // sets the icon
+        Image img = new Image(getClass().getResourceAsStream("/elegit/elegit_icon.png"));
+        primaryStage.getIcons().add(img);
+        // handles mac os dock icon
+        if (SystemUtils.IS_OS_MAC) {
+            java.awt.image.BufferedImage dock_img = ImageIO.read(
+                    getClass().getResourceAsStream(
+                    "/elegit/elegit_icon.png"
+                )
+            );
+            com.apple.eawt.Application.getApplication()
+                    .setDockIconImage(dock_img);
+        }
+
+        primaryStage.setOnCloseRequest(event -> {
+                // On close, upload the logs and delete the log.
                 logger.info("Closed");
-            }
-        });
+                sessionController.submitLog(this.logPath.toString());});
 
         BusyWindow.setParentWindow(primaryStage);
 
         Scene scene = new Scene(root, 1200, 650); // width, height
+
+        // create the menu bar here
+        MenuBar menuBar = MenuPopulator.getInstance().populate();
+        // for now we'll only display menu on mac os
+        // because it blocks repo dropdown menu on other platforms
+        if (SystemUtils.IS_OS_MAC) {
+            ((Pane) scene.getRoot()).getChildren().addAll(menuBar);
+        }
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
