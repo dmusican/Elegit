64e4661176005c3158d8a53099e0b2c43c8803c1
Derek Shang <derekshang07@gmail.com>
2016-05-26 14:58:41 -0500

    Finished unit test on UiUpdate

--- src/test/java/elegit/BranchManagerUiUpdateTest.java
+++ src/test/java/elegit/BranchManagerUiUpdateTest.java
@@ -1,9 +1,12 @@
 package elegit;
 
-import javafx.application.Application;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.MergeCommand;
+import org.eclipse.jgit.api.MergeResult;
+import org.eclipse.jgit.transport.RefSpec;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -13,48 +16,42 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author shangd
  */
 public class BranchManagerUiUpdateTest {
 
-    private static final String REMOTE_URL = "https://bitbucket.org/makik/commitlabeltestrepo.git";
+    private static final String REMOTE_URL = "https://github.com/shangdrk/BranchManagerUiUpdateTestRepo";
     private static final String BRANCH_NAME = "random";
 
-    private CommitTreeModel localCommitTreeModel;
-    private CommitTreeModel remoteCommitTreeModel;
+    Path logPath;
 
     private Path directoryPath;
     private Path repoPath;
     private ClonedRepoHelper helper;
 
-    @BeforeClass
-    public static void setUpJFX() throws Exception{
-        Thread t = new Thread("JavaFX Init Thread") {
-            @Override
-            public void run(){
-                Application.launch(Main.class);
-            }
-        };
-        t.setDaemon(true);
-        t.start();
-
-        // Sleep until the JavaFX environment is up and running
-        Thread.sleep(5000);
-    }
-
     @Before
     public void setUp() throws Exception {
+        initializeLogger();
         cloneTestRepo();
-        openClonedRepo();
     }
 
     @After
     public void tearDown() throws Exception {
         removeAllFilesFromDirectory(directoryPath.toFile());
+    }
+
+    void initializeLogger() {
+        // Create a temp directory for the files to be placed in
+        try {
+            this.logPath = Files.createTempDirectory("elegitLogs");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.logPath.toFile().deleteOnExit();
+        System.setProperty("logFolder", logPath.toString());
     }
 
     void cloneTestRepo() {
@@ -75,36 +72,6 @@ public class BranchManagerUiUpdateTest {
         assertNotNull(helper);
     }
 
-    void openClonedRepo() {
-        localCommitTreeModel = Main.sessionController.localCommitTreeModel;
-        remoteCommitTreeModel = Main.sessionController.remoteCommitTreeModel;
-
-        try {
-            /*new Git(helper.getRepo()).branchCreate()
-                    .setName(BRANCH_NAME)
-                    .setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK)
-                    .setStartPoint("refs/remotes/origin/" + BRANCH_NAME)
-                    .call();*/
-
-            SessionModel.getSessionModel().openRepoFromHelper(helper);
-            helper.fetch();
-            for (RemoteBranchHelper r : helper.getListOfRemoteBranches()) {
-                System.out.println(r.getRefPathString());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        localCommitTreeModel.init();
-        remoteCommitTreeModel.init();
-
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
     void removeAllFilesFromDirectory(File dir) {
         for (File file : dir.listFiles()) {
             if (file.isDirectory()) removeAllFilesFromDirectory(file);
@@ -114,6 +81,21 @@ public class BranchManagerUiUpdateTest {
 
     @Test
     public void testMergeSelectedBranchWithCurrent() throws Exception {
+        // first fetch from remote
+        RefSpec fetchSpec = new RefSpec
+                ("+refs/heads/*:refs/remotes/origin/*");
+        new Git(helper.getRepo()).fetch().setRefSpecs(fetchSpec).call();
+
+        // start tracking the remote branch "random"
+        new Git(helper.getRepo()).checkout().
+                setCreateBranch(true).
+                setForce(true).
+                setName("random").
+                setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK).
+                setStartPoint("origin/random").
+                call();
+
+        // make changes to local file and commit
         File file = Paths.get(this.repoPath.toString(), "test.txt").toFile();
         assertTrue(file.exists());
 
@@ -124,7 +106,24 @@ public class BranchManagerUiUpdateTest {
         this.helper.addFilePath(file.toPath());
         this.helper.commit("Modified test.txt in a unit test!");
 
-        localCommitTreeModel.update();
-        remoteCommitTreeModel.update();
+        new Git(helper.getRepo()).checkout().setName("master").call();
+
+        // merge master into random
+        MergeCommand merge = new Git(helper.getRepo()).merge();
+        merge.include(helper.getRepo().resolve("refs/heads/random"));
+
+        MergeResult mergeResult = merge.call();
+        assertEquals(mergeResult.getMergeStatus(), MergeResult.MergeStatus.FAST_FORWARD);
+
+        CommitTreeController.update(helper);
+
+        // after update, should expect the heads of two branches
+        // are the same commit
+        assertEquals(helper.getListOfLocalBranches().size(), 2);
+
+        String masterHead = helper.getListOfLocalBranches().get(0).getHeadId().getName();
+        String randomHead = helper.getListOfLocalBranches().get(1).getHeadId().getName();
+
+        assertEquals(masterHead, randomHead);
     }
 }
