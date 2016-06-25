550f7290fc8f627bdc56a1104f174c0ae6a809a8
Eric Walker <erictwalker18@gmail.com>
2016-06-25 12:40:38 -0500

    Moving cell service working, still working out kinks in placement algorithm

--- src/main/java/elegit/treefx/TreeLayout.java
+++ src/main/java/elegit/treefx/TreeLayout.java
@@ -30,6 +30,7 @@ public class TreeLayout{
             this.allCellsSortedByTime = allCellsSortedByTime;
             this.max = allCellsSortedByTime.size()-1;
             this.percent = 0;
+            this.currentCell = 0;
         }
 
         public void setCurrentCell(int currentCell) { this.currentCell = currentCell; }
@@ -51,93 +52,6 @@ public class TreeLayout{
         }
     }
 
-
-    // Service to compute the location of where all cells should go
-    public static class ComputeCellPosService extends Service {
-        private List<Integer> maxColumnUsedInRow;
-        private List<Integer> maxColumnWantedInRow;
-        private List<Cell> allCellsSortedByTime;
-        private boolean isInitialSetupFinished;
-        private int cellLocation;
-        private List<Integer> movedCells;
-
-        public ComputeCellPosService(List<Cell> allCellsSortedByTime, boolean isInitialSetupFinished) {
-            this.maxColumnUsedInRow = new ArrayList<>();
-            this.maxColumnWantedInRow = new ArrayList<>();
-
-            this.cellLocation = allCellsSortedByTime.size()-1;
-            this.allCellsSortedByTime = allCellsSortedByTime;
-            this.isInitialSetupFinished = isInitialSetupFinished;
-
-            this.movedCells = new ArrayList<>();
-        }
-
-        public void setCellLocation(int cellLocation) { this.cellLocation = cellLocation;  }
-
-        @Override
-        protected synchronized Task createTask() {
-            return new Task() {
-                @Override
-                protected Void call() throws Exception {
-                    if (cellLocation < 0)
-                        this.cancelled();
-                    if (movedCells.contains(cellLocation))
-                        return null;
-
-                    // Get cell at rightmost location not yet placed
-                    Cell c = allCellsSortedByTime.get(allCellsSortedByTime.size()-1-cellLocation);
-
-                    setCellPosition(c, cellLocation, getRowOfCellInColumn(maxColumnUsedInRow, cellLocation));
-
-                    // Update the reserved columns in rows with the cells parents, oldest to newest
-                    List<Cell> list = c.getCellParents();
-                    list.sort((c1, c2) -> Long.compare(c1.getTime(), c2.getTime()));
-
-                    // For each parent, oldest to newest, place it in the highest row possible recursively
-                    for(Cell parent : list){
-                        int col = allCellsSortedByTime.size() -1- allCellsSortedByTime.indexOf(parent);
-                        int row = getRowOfCellInColumn(maxColumnWantedInRow, col);
-                        if (maxColumnWantedInRow.size() > row)
-                            maxColumnWantedInRow.set(row, col);
-                        else
-                            maxColumnWantedInRow.add(row, col);
-                    }
-                    return null;
-                }
-            };
-        }
-
-        private void setCellPosition(Cell c, int x, int y) {
-            // See whether or not this cell will move
-            int oldColumnLocation = c.columnLocationProperty.get();
-            int oldRowLocation = c.rowLocationProperty.get();
-            c.columnLocationProperty.set(x);
-            c.rowLocationProperty.set(y);
-
-            boolean hasCellMoved = oldColumnLocation >= 0 && oldRowLocation >= 0;
-            boolean willCellMove = oldColumnLocation != x || oldRowLocation != y;
-
-            // Update where the cell has been placed
-            if (y >= maxColumnUsedInRow.size())
-                maxColumnUsedInRow.add(x);
-            else
-                maxColumnUsedInRow.set(y, x);
-
-            // Update the reserved rows
-            for (int i = y+1; i< maxColumnWantedInRow.size(); i++) {
-                if (maxColumnWantedInRow.get(i) == i)
-                    maxColumnWantedInRow.set(i, maxColumnUsedInRow.get(i));
-            }
-
-            // Set the animation and use parent properties of the cell
-            c.setAnimate(isInitialSetupFinished && willCellMove);
-            c.setUseParentAsSource(!hasCellMoved);
-
-            this.movedCells.add(x);
-        }
-    }
-
-
     /**
      * Returns a task that will take care of laying out the given
      * graph into a tree. Uses a combination of recursion and
@@ -152,6 +66,7 @@ public class TreeLayout{
 
             private List<Cell> allCellsSortedByTime;
             private List<Integer> maxColUsedInRow;
+            private List<Integer> movedCells;
             private boolean isInitialSetupFinished;
 
             /**
@@ -168,38 +83,24 @@ public class TreeLayout{
                 sortListOfCells();
 
                 maxColUsedInRow = new ArrayList<>();
-
-                // Spin off thread to compute locations of the cells
-                Thread thread = new Thread(new Task<Void>() {
-                    @Override
-                    protected Void call() throws Exception {
-                        // Set the move cell service to start once the computation of cells finishes
-                        this.setOnSucceeded(event -> {
-                            MoveCellService mover = new MoveCellService(allCellsSortedByTime);
-                            mover.setOnSucceeded(event1 -> {
-                                mover.setCurrentCell(mover.currentCell-1);
-                                mover.restart();
-                            });
-                            mover.setOnCancelled(event1 -> {
-                                treeGraphModel.isInitialSetupFinished = true;
-                            });
-                            mover.start();
-                        });
+                movedCells = new ArrayList<>();
 
 
-                        // Compute the positions of cells recursively
-                        for (int i=allCellsSortedByTime.size()-1; i>=0; i--) {
-                            // Tell the cell to move
-                            computeCellPosition(i);
-                        }
+                // Compute the positions of cells recursively
+                for (int i=allCellsSortedByTime.size()-1; i>=0; i--) {
+                    computeCellPosition(i);
+                }
 
-
-                        return null;
-                    }
+                MoveCellService mover = new MoveCellService(allCellsSortedByTime);
+                mover.setOnSucceeded(event1 -> {
+                    mover.setCurrentCell(mover.currentCell+1);
+                    mover.restart();
                 });
-                thread.setDaemon(true);
-                thread.setName("Cell Position Computing");
-                thread.start();
+                mover.setOnCancelled(event1 -> {
+                    treeGraphModel.isInitialSetupFinished = true;
+                });
+                mover.start();
+
                 return null;
             }
 
@@ -227,6 +128,9 @@ public class TreeLayout{
              * @param cellPosition position of cell to compute position for
              */
             public void computeCellPosition(int cellPosition) {
+                // Don't try to compute a new position if the cell has already been moved
+                if (movedCells.contains(cellPosition))
+                    return;
 
                 // Get cell at rightmost location not yet placed
                 Cell c = allCellsSortedByTime.get(allCellsSortedByTime.size()-1-cellPosition);
@@ -278,11 +182,9 @@ public class TreeLayout{
      */
     private static int getRowOfCellInColumn(List<Integer> maxColumnUsedInRow, int cellCol){
         int row = 0;
-        System.out.println(maxColumnUsedInRow+" "+cellCol);
         while(maxColumnUsedInRow.size() > row && (cellCol > maxColumnUsedInRow.get(row))){
             row++;
         }
-        System.out.println(" "+row+" "+cellCol);
         return row;
     }
 
