864c91288d09c32503fb3a5bc4b96f3474f4b767
Eric Walker <erictwalker18@gmail.com>
2016-07-19 16:01:33 -0500

    Got the basic of the notification bar at the bottom in, messing with css

--- src/main/java/elegit/Main.java
+++ src/main/java/elegit/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.MenuBar;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.apache.commons.lang3.SystemUtils;
