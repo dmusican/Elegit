7f478047661880dde894265b0ec8d21896ead2c9
erictwalker18 <erictwalker18@gmail.com>
2017-04-29 14:32:44 -0700

    Moved AboutController into the controllers folder

--- src/main/java/elegit/controllers/SessionController.java
+++ src/main/java/elegit/controllers/SessionController.java
@@ -19,8 +19,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.*;
 import javafx.scene.control.Label;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
