bbb4f18b4d8d728bb2fd4baa16464b883df3d205
Tate Bosler <tatebosler@gmail.com>
2017-10-13 17:31:49 -0500

    move pre-ssh into its own script

---
+++ travis_ssh.sh
@@ -0,0 +1,7 @@
+sudo ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
+sudo ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+sudo ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+sudo service ssh restart
+
+ssh -T travis@localhost exit
+echo $?
