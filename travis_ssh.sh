9bdd195b0f64c17f22c6997ff4b1d9143163b22a
Tate Bosler <tatebosler@gmail.com>
2017-10-13 17:38:29 -0500

    trying regening the keys not in sudo mode

--- travis_ssh.sh
+++ travis_ssh.sh
@@ -1,7 +1,3 @@
-sudo ssh-keygen -q -f /etc/ssh/ssh_host_key -N '' -t rsa1
-sudo ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-sudo ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
-sudo service ssh restart
-
-ssh -T travis@localhost exit
+ssh-keygen -R localhost
+ssh -T localhost exit
 echo $?
