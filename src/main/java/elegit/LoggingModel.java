c04fb0afda83e008d652428a54f976f5ec9883fa
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2017-10-17 09:36:39 -0500

    DataSubmitter removed (gratutious); moved over to LoggingModel. LoggingModel threadsafe.

--- src/main/java/elegit/LoggingModel.java
+++ src/main/java/elegit/LoggingModel.java
@@ -2,7 +2,16 @@ package elegit;
 
 import elegit.models.PrefObj;
 import javafx.beans.property.*;
+import org.apache.http.HttpEntity;
 import org.apache.http.annotation.GuardedBy;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -10,20 +19,24 @@ import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
-// TODO: Make sure that PrefObj is threadsafe, also DataSubmitter
-// TODO: Make sure that this class if threadsafe
 public class LoggingModel {
 
     private static final Preferences preferences = Preferences.userNodeForPackage(LoggingModel.class);
     private static final String LOGGING_LEVEL_KEY="LOGGING_LEVEL";
     private static final String LAST_UUID_KEY="LAST_UUID";
-    private static final DataSubmitter d = new DataSubmitter();
     private static final Logger logger = LogManager.getLogger();
     @GuardedBy("this") private static final BooleanProperty loggingStatus = new SimpleBooleanProperty(false);
+
+    private static final String submitUrl = "http://elegit.mathcs.carleton.edu/logging/upload.php";
+    private static final String LOG_FILE_NAME = "elegit.log";
 
     public synchronized static void bindLogging(ReadOnlyBooleanProperty status) {
         loggingStatus.bind(status);
@@ -32,7 +45,7 @@ public class LoggingModel {
     public static void submitLog() {
         try {
             String lastUUID = getLastUUID();
-            setLastUUID(d.submitData(lastUUID));
+            setLastUUID(submitData(lastUUID));
         } catch (BackingStoreException | ClassNotFoundException | IOException e) {
             e.printStackTrace();
         } catch (Exception e) {
@@ -104,5 +117,91 @@ public class LoggingModel {
         }
     }
 
+    // Since this deletes a local file, which has a fixed name, this is synchronized to ensure
+    // it doesn't get run more than once simultaneously
+    private synchronized static String submitData(String uuid) {
+        logger.info("Submit data called");
+        String logPath = Paths.get("logs").toString();
+
+        File logDirectory = new File(logPath);
+        File[] logsToUpload=logDirectory.listFiles();
+
+        String lastUUID="";
+        if (uuid==null || uuid.equals("")) {
+            uuid= UUID.randomUUID().toString();
+            logger.info("Making a new uuid.");
+        }
+
+        if (logsToUpload==null)
+            logsToUpload = new File[0];
+        for (File logFile: logsToUpload) {
+            if (!logFile.isFile() || logFile.getName().equals(LOG_FILE_NAME)) {
+                if (logsToUpload.length == 1) {
+                    logger.info("No new logs to upload today");
+                    break;
+                }
+            }
+
+            // Move the file to a uuid filename for upload to the server.
+            try {
+                logFile = Files.copy(logFile.toPath(), logFile.toPath().resolveSibling(uuid+".log")).toFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            logger.info("Attempting to upload log: {}",logFile.getName());
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            try {
+                HttpPost httppost = new HttpPost(submitUrl);
+
+                MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+                builder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
+                FileBody fileBody = new FileBody(logFile);
+
+                builder.addPart("fileToUpload",fileBody);
+                HttpEntity builtEntity = builder.build();
+
+                httppost.setEntity(builtEntity);
+
+                logger.info(httppost.getRequestLine());
+                CloseableHttpResponse response = httpclient.execute(httppost);
+                try {
+                    logger.info("Executing request: " + response.getStatusLine());
+                    logger.info(EntityUtils.toString(response.getEntity()));
+                    lastUUID=uuid;
+                } catch (Exception e) {
+                    logger.error("Response status check failed.");
+                    response.close();
+                    return null;
+                }
+            } catch (Exception e) {
+                logger.error("Failed to execute request. Attempting to close client.");
+                try {
+                    httpclient.close();
+                } catch (Exception f) {
+                    logger.error("Failed to close client.");
+                    return null;
+                }
+                return null;
+            }
+            // Delete the log file as we might be uploading more!
+            if (!logFile.delete()) {
+                logger.error("Failed to delete log file.");
+            }
+
+        }
+        // Clean up the directory
+        File[] logsToDelete = logDirectory.listFiles();
+        if (logsToDelete == null)
+            logsToDelete = new File[0];
+        for (File file: logsToDelete) {
+            if (!file.getName().equals(LOG_FILE_NAME))
+                if (!file.delete()) {
+                    logger.error("Failed to delete a file in the log directory.");
+                }
+        }
+
+        return lastUUID;
+    }
 
 }
