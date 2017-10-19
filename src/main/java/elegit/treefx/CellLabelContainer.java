aa17c7308320766e218bdd46bd7e985a34fd32b0
Dave Musicant at home <dmusican@homemachine>
2017-10-19 14:49:45 -0500

    CellLabelContainer gone through

--- src/main/java/elegit/treefx/CellLabelContainer.java
+++ src/main/java/elegit/treefx/CellLabelContainer.java
@@ -2,6 +2,7 @@ package elegit.treefx;
 
 import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import elegit.Main;
 import elegit.models.RefHelper;
 import elegit.models.TagHelper;
 import io.reactivex.Observable;
@@ -14,6 +15,7 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.transform.Rotate;
+import org.apache.http.annotation.ThreadSafe;
 
 import java.util.*;
 
@@ -22,17 +24,9 @@ import static elegit.treefx.Cell.BOX_SIZE;
 /**
  * Class for the container that shows all the labels for a given cell
  */
+@ThreadSafe
 public class CellLabelContainer extends GridPane {
     private final int MAX_COL_PER_ROW=4;
-
-    HBox basicLabels;
-    List<HBox> extendedLabels;
-
-    /**
-     * Default constructor. Doesn't do anything, but it's nice to have
-     * for blank cell creation.
-     */
-    public CellLabelContainer() { }
 
     /**
      * Translates the container to a given location
@@ -40,20 +34,9 @@ public class CellLabelContainer extends GridPane {
      * @param y the y coordinate of the new location
      */
     public void translate(double x, double y) {
+        Main.assertFxThread();
         setTranslateX(x+BOX_SIZE+10);
         setTranslateY(y+BOX_SIZE-5-(this.getHeight()-25));
-    }
-
-    /**
-     * Helper method to add tooltips to labels that are too long
-     * @param l the label to add a tooltip for
-     * @param text the text of the tooltip
-     */
-    void addToolTip(Label l, String text) {
-        Tooltip tooltip = new Tooltip(text);
-        tooltip.setWrapText(true);
-        tooltip.setMaxWidth(350);
-        Tooltip.install(l, tooltip);
     }
 
     /**
@@ -63,79 +46,78 @@ public class CellLabelContainer extends GridPane {
      * @param cell the cell these labels are associated with
      */
     void setLabels(List<RefHelper> refHelpers, Cell cell) {
-        Platform.runLater(() -> {
-            getChildren().clear();
-            if (refHelpers.size() < 1) {
-                return;
+        Main.assertFxThread();
+        getChildren().clear();
+        if (refHelpers.size() < 1) {
+            return;
+        }
+
+        Label showExtended = new Label();
+        HBox basicLabels = new HBox(5);
+        List<HBox> extendedLabels = new ArrayList<>();
+
+        int col=0;
+        int row=0;
+
+        GridPane.setMargin(basicLabels, new Insets(0,0,5,5));
+        basicLabels.setPickOnBounds(false);
+        for (RefHelper helper : refHelpers) {
+            if (col>MAX_COL_PER_ROW) {
+                row++;
+                col=0;
+                HBox newLine = new HBox(5);
+                GridPane.setMargin(newLine, new Insets(0,0,5,5));
+                GridPane.setRowIndex(newLine, row);
+                newLine.setVisible(false);
+                newLine.setPickOnBounds(false);
+                extendedLabels.add(newLine);
             }
-
-            Label showExtended = new Label();
-            basicLabels = new HBox(5);
-            extendedLabels = new ArrayList<>();
-
-            int col=0;
-            int row=0;
-
-            GridPane.setMargin(basicLabels, new Insets(0,0,5,5));
-            basicLabels.setPickOnBounds(false);
-            for (RefHelper helper : refHelpers) {
-                if (col>MAX_COL_PER_ROW) {
-                    row++;
-                    col=0;
-                    HBox newLine = new HBox(5);
-                    GridPane.setMargin(newLine, new Insets(0,0,5,5));
-                    GridPane.setRowIndex(newLine, row);
-                    newLine.setVisible(false);
-                    newLine.setPickOnBounds(false);
-                    extendedLabels.add(newLine);
-                }
-                CellLabel label;
-                if (helper instanceof TagHelper) {
-                    label = new TagCellLabel(helper, false);
-                }
-                else
-                    label = new BranchCellLabel(helper, false);
-
-                if (row>0) {
-                    extendedLabels.get(row-1).getChildren().add(label);
-                } else {
-                    basicLabels.getChildren().add(label);
-                }
-                col++;
+            CellLabel label;
+            if (helper instanceof TagHelper) {
+                label = new TagCellLabel(helper, false);
             }
+            else
+                label = new BranchCellLabel(helper, false);
 
-            showExtended.setVisible(false);
             if (row>0) {
-                showExtended.setVisible(true);
-                showExtended.setTranslateX(-6);
-                showExtended.setTranslateY(-3);
-                Node down = GlyphsDude.createIcon(FontAwesomeIcon.CARET_DOWN);
-                Node up = GlyphsDude.createIcon(FontAwesomeIcon.CARET_UP);
-                showExtended.setGraphic(down);
-                showExtended.setOnMouseClicked(event -> {
-                    if(showExtended.getGraphic().equals(down)) {
-                        showExtended.setGraphic(up);
-                    }else {
-                        showExtended.setGraphic(down);
-                    }
-                    for (Node n : extendedLabels) {
-                        n.setVisible(!n.isVisible());
-                    }
-                });
+                extendedLabels.get(row-1).getChildren().add(label);
+            } else {
+                basicLabels.getChildren().add(label);
             }
+            col++;
+        }
 
-            // We rotate the labels because it's more efficient than having our tree
-            // upside down and moving everything around often.
-            this.setMaxHeight(20);
-            this.setRotationAxis(Rotate.X_AXIS);
-            this.setRotate(180);
-            this.visibleProperty().bind(cell.visibleProperty());
+        showExtended.setVisible(false);
+        if (row>0) {
+            showExtended.setVisible(true);
+            showExtended.setTranslateX(-6);
+            showExtended.setTranslateY(-3);
+            Node down = GlyphsDude.createIcon(FontAwesomeIcon.CARET_DOWN);
+            Node up = GlyphsDude.createIcon(FontAwesomeIcon.CARET_UP);
+            showExtended.setGraphic(down);
+            showExtended.setOnMouseClicked(event -> {
+                if(showExtended.getGraphic().equals(down)) {
+                    showExtended.setGraphic(up);
+                }else {
+                    showExtended.setGraphic(down);
+                }
+                for (Node n : extendedLabels) {
+                    n.setVisible(!n.isVisible());
+                }
+            });
+        }
 
-            getChildren().clear();
-            getChildren().addAll(basicLabels);
-            getChildren().addAll(extendedLabels);
-            getChildren().add(showExtended);
-        });
+        // We rotate the labels because it's more efficient than having our tree
+        // upside down and moving everything around often.
+        this.setMaxHeight(20);
+        this.setRotationAxis(Rotate.X_AXIS);
+        this.setRotate(180);
+        this.visibleProperty().bind(cell.visibleProperty());
+
+        getChildren().clear();
+        getChildren().addAll(basicLabels);
+        getChildren().addAll(extendedLabels);
+        getChildren().add(showExtended);
         this.setPickOnBounds(false);
     }
 
@@ -144,24 +126,23 @@ public class CellLabelContainer extends GridPane {
      * @param labels the labels that refer to the current refs
      */
     void setCurrentLabels(Set<String> labels) {
-        Platform.runLater(() -> {
-            for (Node m : getChildren()) {
-                if (m instanceof HBox) {
+        Main.assertFxThread();
+        for (Node m : getChildren()) {
+            if (m instanceof HBox) {
 
-                    // Buffer them 10 at a time so as not to flood FX queue
-                    Observable.fromIterable(((HBox) m).getChildren())
-                            .buffer(10)
-                            .subscribe(nodes -> {
-                                Platform.runLater( () -> {
-                                    for (Node n : nodes) {
-                                        if (n instanceof CellLabel && labels.contains(((CellLabel) n).getLabel().getText()))
-                                            ((CellLabel) n).setCurrent(true);
-                                    }
-                                });
-                            }, Throwable::printStackTrace);
-                }
+                // Buffer them 10 at a time so as not to flood FX queue
+                Observable.fromIterable(((HBox) m).getChildren())
+                        .buffer(10)
+                        .subscribe(nodes -> {
+                            Platform.runLater( () -> {
+                                for (Node n : nodes) {
+                                    if (n instanceof CellLabel && labels.contains(((CellLabel) n).getLabel().getText()))
+                                        ((CellLabel) n).setCurrent(true);
+                                }
+                            });
+                        }, Throwable::printStackTrace);
             }
-        });
+        }
     }
 
     /**
@@ -169,17 +150,16 @@ public class CellLabelContainer extends GridPane {
      * @param menuMap a map between ref helpers and context menus
      */
     void setLabelMenus(Map<RefHelper, ContextMenu> menuMap) {
-        Platform.runLater(() -> {
-            for (Node m : getChildren()) {
-                if (m instanceof HBox) {
-                    for (Node n : ((HBox) m).getChildren()) {
-                        if (n instanceof CellLabel && menuMap.keySet().contains(((CellLabel) n).getRefHelper())) {
-                            ((CellLabel) n).setContextMenu(menuMap.get(((CellLabel) n).getRefHelper()));
-                        }
+        Main.assertFxThread();
+        for (Node m : getChildren()) {
+            if (m instanceof HBox) {
+                for (Node n : ((HBox) m).getChildren()) {
+                    if (n instanceof CellLabel && menuMap.keySet().contains(((CellLabel) n).getRefHelper())) {
+                        ((CellLabel) n).setContextMenu(menuMap.get(((CellLabel) n).getRefHelper()));
                     }
                 }
             }
-        });
+        }
     }
 
     /**
@@ -187,16 +167,15 @@ public class CellLabelContainer extends GridPane {
      * @param labels the labels to set as remote
      */
     void setRemoteLabels(List<String>  labels) {
-        Platform.runLater(() -> {
-            for (Node m : getChildren()) {
-                if (m instanceof HBox) {
-                    for (Node n : ((HBox) m).getChildren()) {
-                        if (n instanceof CellLabel && labels.contains(((CellLabel) n).getLabel().getText())) {
-                            ((CellLabel) n).setRemote(true);
-                        }
+        Main.assertFxThread();
+        for (Node m : getChildren()) {
+            if (m instanceof HBox) {
+                for (Node n : ((HBox) m).getChildren()) {
+                    if (n instanceof CellLabel && labels.contains(((CellLabel) n).getLabel().getText())) {
+                        ((CellLabel) n).setRemote(true);
                     }
                 }
             }
-        });
+        }
     }
 }
