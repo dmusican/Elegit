beeb68299a04263d27855bd0c29abc42776d048f
Dave Musicant at home <dmusican@homemachine>
2017-10-25 11:39:10 -0500

    Label height lined up

--- src/main/java/elegit/treefx/CellLabelContainer.java
+++ src/main/java/elegit/treefx/CellLabelContainer.java
@@ -36,7 +36,7 @@ public class CellLabelContainer extends GridPane {
     public void translate(double x, double y) {
         Main.assertFxThread();
         setTranslateX(x+BOX_SIZE+10);
-        setTranslateY(y+BOX_SIZE-5-(this.getHeight()-25));
+        setTranslateY(y+BOX_SIZE-this.getHeight()-2);
     }
 
     /**
