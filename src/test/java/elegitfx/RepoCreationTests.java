83048f944b5ca7e3433ec43a0e1e24c27f51c04c
Dave Musicant at home <dmusican@homemachine>
2017-11-15 14:22:23 -0600

    Got a test to fail at nodes not being cleared bug. Now on to fix.

--- src/test/java/elegitfx/RepoCreationTests.java
+++ src/test/java/elegitfx/RepoCreationTests.java
@@ -3,6 +3,10 @@ package elegitfx;
 import elegit.Main;
 import elegit.controllers.BusyWindow;
 import elegit.controllers.SessionController;
+import elegit.exceptions.CancelledAuthorizationException;
+import elegit.exceptions.MissingRepoException;
+import elegit.exceptions.NoCommitsToPushException;
+import elegit.exceptions.PushToAheadRemoteError;
 import elegit.models.BranchHelper;
 import elegit.models.BranchModel;
 import elegit.models.ExistingRepoHelper;
@@ -25,6 +29,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.PushCommand;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.hamcrest.Matchers;
 import org.junit.After;
@@ -39,6 +44,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.concurrent.CountDownLatch;
 import java.util.prefs.Preferences;
 
 import static junit.framework.TestCase.assertEquals;
@@ -258,6 +264,86 @@ public class RepoCreationTests extends ApplicationTest {
         });
 
 
+    }
+
+
+
+
+    @Test
+    public void countOfCommitsInTreeTest() throws Exception {
+
+        // Make two repos; swap between them, make sure number of commits is correct in tree
+        logger.info("Temp directory: " + directoryPath);
+
+        Path remote1 = directoryPath.resolve("remote1");
+        Path local1 = directoryPath.resolve("local1");
+        RevCommit firstCommit1 = makeTestRepo(remote1, local1, 5);
+        logger.info(remote1);
+        logger.info(local1);
+
+        Path remote2 = directoryPath.resolve("remote2");
+        Path local2 = directoryPath.resolve("local2");
+        RevCommit firstCommit2 = makeTestRepo(remote2, local2, 5);
+        logger.info(remote2);
+        logger.info(local2);
+
+        SessionController.gitStatusCompletedOnce = new CountDownLatch(1);
+
+        clickOn("#loadNewRepoButton")
+                .clickOn("#loadExistingRepoOption")
+                .clickOn("#repoInputDialog")
+                .write(local1.toString())
+                .clickOn("#repoInputDialogOK");
+
+        SessionController.gitStatusCompletedOnce.await();
+
+        Cell firstCell1 = lookup(Matchers.hasToString(firstCommit1.getName())).query();
+        assertNotEquals(null, firstCell1);
+
+        clickOn("#loadNewRepoButton")
+                .clickOn("#loadExistingRepoOption")
+                .clickOn("#repoInputDialog")
+                .write(local2.toString())
+                .clickOn("#repoInputDialogOK");
+
+        Cell firstCell2 = lookup(Matchers.hasToString(firstCommit2.getName())).query();
+        assertNotEquals(null, firstCell2);
+
+        // Should no longer be there
+        firstCell1 = lookup(Matchers.hasToString(firstCommit1.getName())).query();
+        assertEquals(null, firstCell1);
+
+    }
+
+    private RevCommit makeTestRepo(Path remote, Path local, int numCommits) throws GitAPIException, IOException, CancelledAuthorizationException, MissingRepoException, PushToAheadRemoteError, NoCommitsToPushException {
+        Git.init().setDirectory(remote.toFile()).setBare(true).call();
+        Git.cloneRepository().setDirectory(local.toFile()).setURI("file://" + remote).call();
+
+        ExistingRepoHelper helper = new ExistingRepoHelper(local, new ElegitUserInfoTest());
+
+        Path fileLocation = local.resolve("README.md");
+
+        FileWriter fw = new FileWriter(fileLocation.toString(), true);
+        fw.write("start");
+        fw.close();
+        helper.addFilePathTest(fileLocation);
+        RevCommit firstCommit = helper.commit("Appended to file");
+        Cell firstCellAttempt = lookup(firstCommit.getName()).query();
+        logger.info("firstCell = " + firstCellAttempt);
+
+        for (int i = 0; i < numCommits; i++) {
+            fw = new FileWriter(fileLocation.toString(), true);
+            fw.write("" + i);
+            fw.close();
+            helper.addFilePathTest(fileLocation);
+            helper.commit("Appended to file");
+        }
+
+        // Just push all untracked local branches
+        PushCommand command = helper.prepareToPushAll(untrackedLocalBranches -> untrackedLocalBranches);
+        helper.pushAll(command);
+
+        return firstCommit;
     }
 
 }
