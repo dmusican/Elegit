0e05e5f75779577613b2b5d3bd959776e17d4447
Dave Musicant at home <dmusican@homemachine>
2017-11-30 14:39:07 -0600

    Redid some tests, trying to get all to run headless

--- src/test/java/elegitfx/PushPullTestFX.java
+++ src/test/java/elegitfx/PushPullTestFX.java
@@ -1,18 +1,20 @@
-package elegit;
+package elegitfx;
 
+import elegit.Main;
 import elegit.models.ClonedRepoHelper;
 import elegit.models.ExistingRepoHelper;
 import elegit.models.SessionModel;
 import elegit.monitors.RepositoryMonitor;
 import elegit.sshauthentication.ElegitUserInfoTest;
 import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
 import org.eclipse.jgit.api.PushCommand;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.framework.junit.ApplicationTest;
 
-import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -22,7 +24,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch;
 
 import static elegit.monitors.RepositoryMonitor.REMOTE_CHECK_INTERVAL;
 import static org.junit.Assert.assertFalse;
@@ -32,7 +33,7 @@ import static org.junit.Assert.assertTrue;
 /**
  * Created by dmusican. Enjoy!
  */
-public class PushPullTest {
+public class PushPullTestFX extends ApplicationTest {
 
 
     private Path directoryPath;
@@ -41,6 +42,7 @@ public class PushPullTest {
 
     // Used to indicate that if password files are missing, then tests should just pass
     private boolean looseTesting;
+
 
 
 
@@ -81,10 +83,11 @@ public class PushPullTest {
         }
     }
 
-
-
     @Test
-    public void testPushPullBothClonedExisting() throws Exception {
+    // This test has some thread safety issues that should probably be fixed; the RepositoryMonitor is started
+    // in the FX thread (which is right), but there's a lot of code here in this test not run in the FX thread
+    // that probably should be. Fix it if this test starts causing trouble. In the meantime... it's a test.
+    public void testPushPullBothCloned() throws Exception {
         File authData = new File(testFileLocation + "httpUsernamePassword.txt");
 
         // If a developer does not have this file present, test should just pass.
@@ -107,47 +110,24 @@ public class PushPullTest {
 
         // Repo that will pull
         Path repoPathPull = directoryPath.resolve("pushpull2");
-        ClonedRepoHelper clonedHelperPull = new ClonedRepoHelper(repoPathPull, remoteURL, credentials);
-        assertNotNull(clonedHelperPull);
-        clonedHelperPull.obtainRepository(remoteURL);
-        ExistingRepoHelper existingHelperPull = new ExistingRepoHelper(repoPathPull, new ElegitUserInfoTest());
+        ClonedRepoHelper helperPull = new ClonedRepoHelper(repoPathPull, remoteURL, credentials);
+        assertNotNull(helperPull);
+        helperPull.obtainRepository(remoteURL);
 
-        // Update the file, then commit and push
-        Path readmePath = repoPathPush.resolve("README.md");
-        System.out.println(readmePath);
-        String timestamp = "testPushPullBothClonedExisting " + (new Date()).toString() + "\n";
-        Files.write(readmePath, timestamp.getBytes(), StandardOpenOption.APPEND);
-        helperPush.addFilePathTest(readmePath);
-        helperPush.commit("added a character");
-        PushCommand command = helperPush.prepareToPushAll();
-        helperPush.pushAll(command);
+        Platform.runLater(() -> {
+            try {
+                // Create a session model for helperPull so can later verify if changes have been seen
+                SessionModel model = SessionModel.getSessionModel();
+                model.openRepoFromHelper(helperPull);
+                RepositoryMonitor.initRemote();
+            } catch (Exception e) {
+                throw new AssertionError(e);
+            }
+        });
 
-        // Now do the pull (well, a fetch)
-        existingHelperPull.fetch(false);
-        existingHelperPull.mergeFromFetch();
-    }
-
-    @Test
-    public void cloneThenPushTestWithoutAuthentication() throws Exception {
-        File authData = new File(testFileLocation + "httpUsernamePassword.txt");
-
-        // If a developer does not have this file present, test should just pass.
-        if (!authData.exists() && looseTesting)
-            return;
-
-        Scanner scanner = new Scanner(authData);
-        String ignoreURL = scanner.next();
-        String username = scanner.next();
-        String password = scanner.next();
-        UsernamePasswordCredentialsProvider credentials = new UsernamePasswordCredentialsProvider(username, password);
-
-        String remoteURL = "https://github.com/TheElegitTeam/PushPullTests.git";
-        //String remoteURL = "https://github.com/connellyj/HelloWorld.git";
-
-        Path repoPathPush = directoryPath.resolve("clonepush");
-        ClonedRepoHelper helperPush = new ClonedRepoHelper(repoPathPush, remoteURL, credentials);
-        assertNotNull(helperPush);
-        helperPush.obtainRepository(remoteURL);
+        // The RepositoryMonitor posts updates to hasFoundNewRemoteChanges to the FX thread, so need to run
+        // this separately to be able to observe changes
+        assertFalse(RepositoryMonitor.hasFoundNewRemoteChanges.get());
 
         // Update the file, then commit and push
         Path readmePath = repoPathPush.resolve("README.md");
@@ -156,10 +136,38 @@ public class PushPullTest {
         Files.write(readmePath, timestamp.getBytes(), StandardOpenOption.APPEND);
         helperPush.addFilePathTest(readmePath);
         helperPush.commit("added a character");
-        PushCommand push = helperPush.prepareToPushAll();
-        helperPush.pushAll(push);
+        PushCommand command = helperPush.prepareToPushAll();
+        helperPush.pushAll(command);
 
+        // Verify that RepositoryMonitor can see the changes relative to the original
+        Thread.sleep(REMOTE_CHECK_INTERVAL);
+        assertTrue(RepositoryMonitor.hasFoundNewRemoteChanges.get());
+
+        // Add a tag named for the current timestamp
+        ObjectId headId = helperPush.getBranchModel().getCurrentBranch().getHeadId();
+        String tagName = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmSSS"));
+        assertFalse(helperPush.getCommit(headId).hasTag(tagName));
+        helperPush.getTagModel().tag(tagName, headId.name());
+        assertTrue(helperPush.getCommit(headId).hasTag(tagName));
+        helperPush.pushTags();
+
+        // Remove the tag we just added
+        helperPush.getTagModel().deleteTag(tagName);
+        assertFalse(helperPush.getCommit(headId).hasTag(tagName));
+        command = helperPush.prepareToPushAll();
+        command.setRemote("origin").add(":refs/tags/" + tagName);
+        helperPush.pushAll(command);
+
+        // Now do the pull (well, a fetch)
+        helperPull.fetch(false);
+        helperPull.mergeFromFetch();
+
+        // Update lists of branches
+        helperPull.getBranchModel().updateAllBranches();
+        System.out.println("done");
     }
+
+
 
 
 }
