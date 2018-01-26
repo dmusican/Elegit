065b140690588d3dc8e6dce9a7e3c7d0f28c5f93
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2018-01-26 14:14:29 -0600

    Test which is throwing exceptions when try to interrupt prompt

---
+++ src/test/java/elegitfx/SshPopupInterruptTest.java
@@ -0,0 +1,120 @@
+package elegitfx;
+
+import elegit.Main;
+import elegit.controllers.SessionController;
+import elegit.exceptions.ExceptionAdapter;
+import elegit.models.ClonedRepoHelper;
+import elegit.models.ExistingRepoHelper;
+import elegit.monitors.RepositoryMonitor;
+import elegit.sshauthentication.ElegitUserInfoGUI;
+import elegit.sshauthentication.ElegitUserInfoTest;
+import io.reactivex.Completable;
+import io.reactivex.schedulers.Schedulers;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import junit.framework.TestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.sshd.server.SshServer;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.util.WaitForAsyncUtils;
+import sharedrules.TestUtilities;
+import sharedrules.TestingLogPathRule;
+import sharedrules.TestingRemoteAndLocalReposRule;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class SshPopupInterruptTest extends ApplicationTest {
+
+    @ClassRule
+    public static final TestingLogPathRule testingLogPath = new TestingLogPathRule();
+
+    @Rule
+    public final TestingRemoteAndLocalReposRule testingRemoteAndLocalRepos =
+            new TestingRemoteAndLocalReposRule(false);
+    private Path directoryPath;
+
+    private static final Logger console = LogManager.getLogger("briefconsolelogger");
+
+
+
+
+    static {
+        // -----------------------Logging Initialization Start---------------------------
+        Path logPath = Paths.get("logs");
+        String s = logPath.toAbsolutePath().toString();
+        System.setProperty("logFolder", s);
+    }
+
+    private static final Logger logger = LogManager.getLogger("consolelogger");
+
+
+    private SessionController sessionController;
+
+    @Rule
+    public TestName testName = new TestName();
+
+    @Before
+    public void setup() throws Exception {
+        logger.info("Unit test started");
+        initializeLogger();
+        directoryPath = testingRemoteAndLocalRepos.getDirectoryPath();
+    }
+
+
+    // Helper method to avoid annoying traces from logger
+    void initializeLogger() throws IOException {
+        // Create a temp directory for the files to be placed in
+        Path logPath = Files.createTempDirectory("elegitLogs");
+        logPath.toFile().deleteOnExit();
+        System.setProperty("logFolder", logPath.toString());
+    }
+
+    @After
+    public void tearDown() {
+        logger.info("Tearing down");
+        TestCase.assertEquals(0, Main.getAssertionCount());
+    }
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        sessionController = TestUtilities.commonTestFxStart(stage);
+    }
+
+    @Test
+    public void test() throws Exception {
+
+
+        ElegitUserInfoGUI userInfo = new ElegitUserInfoGUI();
+        Thread t1 = new Thread(() -> userInfo.promptPassphrase("passphrase prompt"));
+
+        t1.start();
+
+        WaitForAsyncUtils.waitFor(10, TimeUnit.SECONDS,
+                                  () -> lookup("#sshprompt").query() != null);
+
+        t1.interrupt();
+
+/
+    }
+
+}
