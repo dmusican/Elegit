8907d39fe09a5600e3c4dc6c082f3d5fd079d341
Miriam <yamgorra@gmail.com>
2018-06-26 13:45:34 -0500

    moving code around

--- src/main/java/elegit/controllers/ConflictManagementToolController.java
+++ src/main/java/elegit/controllers/ConflictManagementToolController.java
@@ -126,10 +126,12 @@ public class ConflictManagementToolController {
     private static final Logger console = LogManager.getLogger("briefconsolelogger");
 
     synchronized void setSessionController(SessionController sessionController) {
+        Main.assertFxThread();
         this.sessionController = sessionController;
     }
 
     public void initialize() {
+        Main.assertFxThread();
         mergeResult=SessionModel.getSessionModel().getMergeResult();
         initButtons();
         initDropdown();
@@ -144,6 +146,7 @@ public class ConflictManagementToolController {
      * Add graphics and tool tips for all of the buttons in the tool.
      */
     private void initButtons() {
+        Main.assertFxThread();
         // Accept change buttons
         initButton(FontAwesomeIcon.CHECK, "checkIcon", leftAccept, "Integrate the highlighted commit.");
         initButton(FontAwesomeIcon.CHECK, "checkIcon", rightAccept, "Integrate the highlighted commit.");
@@ -166,6 +169,7 @@ public class ConflictManagementToolController {
     }
 
     private void initButton(GlyphIcons glyphIcon, String id, Button button, String toolTip) {
+        Main.assertFxThread();
         Text icon = GlyphsDude.createIcon(glyphIcon);
         icon.setId(id);
         button.setGraphic(icon);
@@ -173,6 +177,7 @@ public class ConflictManagementToolController {
     }
 
     private void initDropdown() {
+        Main.assertFxThread();
         try {
             // Get the names of all the conflicting files and put them in the dropdown
             Set<String> conflictingFiles = SessionModel.getSessionModel().getConflictingFiles(null);
@@ -187,6 +192,7 @@ public class ConflictManagementToolController {
     }
 
     private void initCodeAreas() {
+        Main.assertFxThread();
         // Add line numbers and pointers to each CodeArea
         addLineNumbersAndPointers(leftDoc);
         addLineNumbersAndPointers(middleDoc);
@@ -205,6 +211,7 @@ public class ConflictManagementToolController {
     }
 
     private void addLineNumbersAndPointers(CodeArea doc) {
+        Main.assertFxThread();
         IntFunction<Node> numberFactory = LineNumberFactory.get(doc);
         IntFunction<Node> conflictLinePointer = new ConflictLinePointer(doc.currentParagraphProperty());
         IntFunction<Node> graphicFactory = line -> {
@@ -218,14 +225,17 @@ public class ConflictManagementToolController {
     }
 
     private void bindHorizontalScroll(CodeArea doc1, CodeArea doc2) {
+        Main.assertFxThread();
         doc1.estimatedScrollXProperty().bindBidirectional(doc2.estimatedScrollXProperty());
     }
 
     private void bindVerticalScroll(CodeArea doc1, CodeArea doc2) {
+        Main.assertFxThread();
         doc1.estimatedScrollYProperty().bindBidirectional(doc2.estimatedScrollYProperty());
     }
 
     private void setButtonsDisabled(boolean disabled) {
+        Main.assertFxThread();
         leftAccept.setDisable(disabled);
         leftReject.setDisable(disabled);
         leftUndo.setDisable(disabled);
@@ -241,6 +251,7 @@ public class ConflictManagementToolController {
      * Shows the tool.
      */
     void showStage(AnchorPane pane) {
+        Main.assertFxThread();
         anchorRoot = pane;
         stage = new Stage();
         stage.setTitle("Conflict Management Tool");
@@ -280,12 +291,14 @@ public class ConflictManagementToolController {
 
     @FXML
     private void handleAbort() {
+        Main.assertFxThread();
         logger.info("Conflict management session aborted.");
         stage.close();
     }
 
     @FXML
     private void handleApplyChanges() {
+        Main.assertFxThread();
         logger.info("Changes made with the conflict management tool applied.");
         if (conflictsLeftToHandle != 0) {
             showNotAllConflictHandledNotification();
@@ -310,6 +323,7 @@ public class ConflictManagementToolController {
 
     @FXML
     private void handleToggleUp() {
+        Main.assertFxThread();
         logger.info("Toggled up.");
         setModifyingButtonsDisabled(false);
 
@@ -327,6 +341,7 @@ public class ConflictManagementToolController {
 
     @FXML
     private void handleToggleDown() {
+        Main.assertFxThread();
         logger.info("Toggle down.");
         setModifyingButtonsDisabled(false);
 
@@ -343,18 +358,21 @@ public class ConflictManagementToolController {
     }
 
     private void moveDocCaretsToLastConflict() {
+        Main.assertFxThread();
         moveDocCarets(leftDoc, leftConflictingLineNumbers.get(leftConflictingLineNumbers.size() - 1));
         moveDocCarets(middleDoc, middleConflictingLineNumbers.get(middleConflictingLineNumbers.size() - 1));
         moveDocCarets(rightDoc, rightConflictingLineNumbers.get(rightConflictingLineNumbers.size() - 1));
     }
 
     private void moveDocCaretsToFirstConflict() {
+        Main.assertFxThread();
         moveDocCarets(leftDoc, leftConflictingLineNumbers.get(0));
         moveDocCarets(middleDoc, middleConflictingLineNumbers.get(0));
         moveDocCarets(rightDoc, rightConflictingLineNumbers.get(0));
     }
 
     private void findConflictToGoTo(int currentLine, boolean up) {
+        Main.assertFxThread();
         for (int i = 0; i < middleConflictingLineNumbers.size(); i++) {
             if (up) { // Toggling up
                 if (currentLine <= middleConflictingLineNumbers.get(i)) {
@@ -374,42 +392,49 @@ public class ConflictManagementToolController {
     }
 
     private void moveDocCaretsGivenDirection(int i, int direction) {
+        Main.assertFxThread();
         moveDocCarets(leftDoc, leftConflictingLineNumbers.get(i + direction));
         moveDocCarets(middleDoc, middleConflictingLineNumbers.get(i + direction));
         moveDocCarets(rightDoc, rightConflictingLineNumbers.get(i + direction));
     }
 
     private void moveDocCarets(CodeArea doc, int lineNumber) {
+        Main.assertFxThread();
         doc.moveTo(lineNumber, 0);
         doc.requestFollowCaret();
     }
 
     @FXML
     private void handleAcceptLeftChange() {
+        Main.assertFxThread();
         logger.info("Change on left document accepted.");
         handleChange(leftDoc, leftConflictingLineNumbers, leftConflictLines, true);
     }
 
     @FXML
     private void handleAcceptRightChange() {
+        Main.assertFxThread();
         logger.info("Change on right document accepted.");
         handleChange(rightDoc, rightConflictingLineNumbers, rightConflictLines, true);
     }
 
     @FXML
     private void handleRejectLeftChange() {
+        Main.assertFxThread();
         logger.info("Change on left document rejected.");
         handleChange(leftDoc, leftConflictingLineNumbers, leftConflictLines, false);
     }
 
     @FXML
     private void handleRejectRightChange() {
+        Main.assertFxThread();
         logger.info("Change on right document rejected.");
         handleChange(rightDoc, rightConflictingLineNumbers, rightConflictLines, false);
     }
 
     private void handleChange(CodeArea doc, ArrayList<Integer> conflictingLineNumbers,
                               ArrayList<ConflictLine> conflictLines, boolean accepting) {
+        Main.assertFxThread();
         int currentLine = doc.getCurrentParagraph();
 
         for (int conflictLineIndex = 0; conflictLineIndex < conflictingLineNumbers.size(); conflictLineIndex++) {
@@ -434,6 +459,7 @@ public class ConflictManagementToolController {
     }
 
     private void updateMiddleDoc(ArrayList<ConflictLine> conflictLines, int conflictLineIndex) {
+        Main.assertFxThread();
         for (String line : conflictLines.get(conflictLineIndex).getLines()) {
             updateCurrentLine(line);
             middleConflictLines.get(conflictLineIndex).addLine(line);
@@ -444,6 +470,7 @@ public class ConflictManagementToolController {
     }
 
     private void updateCurrentLine(String line) {
+        Main.assertFxThread();
         int startIndex = middleDoc.getCaretPosition();
         middleDoc.insertText(middleDoc.getCurrentParagraph(), 0, line + "\n");
         int endIndex = middleDoc.getCaretPosition();
@@ -451,12 +478,14 @@ public class ConflictManagementToolController {
     }
 
     private void updateMiddleConflictingLineNumbers(int numLines, int conflictLineIndex) {
+        Main.assertFxThread();
         for (int i = conflictLineIndex + 1; i < middleConflictingLineNumbers.size(); i++) {
             middleConflictingLineNumbers.set(i, middleConflictingLineNumbers.get(i) + numLines);
         }
     }
 
     private void updateSideDoc(CodeArea doc, int startOfConflict, int numLines) {
+        Main.assertFxThread();
         int startIndex = doc.getCaretPosition();
         doc.moveTo(startOfConflict + numLines, 0);
         int endIndex = doc.getCaretPosition();
@@ -466,6 +495,7 @@ public class ConflictManagementToolController {
 
     // TODO: figure out what flags to change
     private void updateConflictLineStatus(ArrayList<ConflictLine> conflictLines, int conflictLineIndex) {
+        Main.assertFxThread();
 //        middleConflictLines.get(conflictLineIndex).setChangedStatus(true);
         middleConflictLines.get(conflictLineIndex).setConflictStatus(false);
 //        conflictLines.get(conflictLineIndex).setChangedStatus(true);
@@ -473,6 +503,7 @@ public class ConflictManagementToolController {
     }
 
     private void updateAndCheckConflictsLeftToHandle() {
+        Main.assertFxThread();
         conflictsLeftToHandle--;
         if (conflictsLeftToHandle == 0) {
             showAllConflictsHandledNotification();
@@ -480,6 +511,7 @@ public class ConflictManagementToolController {
     }
 
     private void showAcceptOrRejectWarning(boolean accepting) {
+        Main.assertFxThread();
         if (accepting) {
             showAttemptingToAcceptANonConflictNotification();
         } else {
@@ -489,11 +521,13 @@ public class ConflictManagementToolController {
 
     @FXML
     private synchronized void handleUndoLeftChange() {
+        Main.assertFxThread();
         logger.info("Modification undone from left document.");
     }
 
     @FXML
     private synchronized void handleUndoRightChange() {
+        Main.assertFxThread();
         logger.info("Modification undone from right document.");
     }
 
@@ -502,6 +536,7 @@ public class ConflictManagementToolController {
 
     @FXML
     private void setFileToEdit() {
+        Main.assertFxThread();
         logger.info("New file selected to edit in conflict management tool.");
         Main.assertFxThread();
         setDropdownValueToFileName();
@@ -514,12 +549,14 @@ public class ConflictManagementToolController {
     }
 
     private void setDropdownValueToFileName() {
+        Main.assertFxThread();
         // Show file in dropdown
         EventHandler<ActionEvent> handler = conflictingFilesDropdown.getOnAction();
         conflictingFilesDropdown.setOnAction(handler);
     }
 
     private void clearTextAreas() {
+        Main.assertFxThread();
         // Clear the documents before loading new ones
         leftDoc.clear();
         middleDoc.clear();
@@ -527,6 +564,7 @@ public class ConflictManagementToolController {
     }
 
     private void updateTextAreasWithNewFile() {
+        Main.assertFxThread();
         // Get the path of the selected file and the name of the file
         Path directory = (new File(SessionModel.getSessionModel().getCurrentRepoHelper().getRepo().getDirectory()
                 .getParent())).toPath();
@@ -538,17 +576,21 @@ public class ConflictManagementToolController {
     }
 
     private void setLabels(ConflictManagementModel conflictManagementModel) {
+        Main.assertFxThread();
         leftDocLabel.setText(mergeResult.get("baseBranch"));
         middleDocLabel.setText("Result");
         rightDocLabel.setText(mergeResult.get("mergedBranch"));
     }
 
     public void setFile(String filePathWithoutFileName, String fileName) {
+        Main.assertFxThread();
         fileSelected = true;
         conflictingFilesDropdown.setPromptText(fileName);
         ConflictManagementModel conflictManagementModel = new ConflictManagementModel();
-        ArrayList<ArrayList> results = conflictManagementModel.parseConflicts(filePathWithoutFileName +
-                File.separator + fileName, getBaseParentFiles(fileName), getMergedParentFiles(fileName));
+        ArrayList<String> base = getBaseParentFiles(fileName);
+        System.out.println(base);
+        ArrayList<ArrayList> results = conflictManagementModel.parseConflicts(fileName,
+                filePathWithoutFileName, mergeResult.get("baseParent"), mergeResult.get("mergedParent"));
 
         setLabels(conflictManagementModel);
 
@@ -567,7 +609,6 @@ public class ConflictManagementToolController {
         CodeArea middle = setLines(middleAllConflictLines, middleDoc);
         setLines(rightAllConflictLines, rightDoc);
         files.put(fileName, middle);
-        //getParentFiles(fileName);
         // Allow the user to click buttons
         setButtonsDisabled(false);
         // Move the caret and doc to the first conflict
@@ -581,8 +622,10 @@ public class ConflictManagementToolController {
     }
 
     private ArrayList<String> getParentFiles(ObjectId parent, String fileName){
+        Main.assertFxThread();
         try{
             Repository repository = SessionModel.getSessionModel().getCurrentRepoHelper().getRepo();
+            System.out.println(repository);
             RevWalk revWalk = new RevWalk(repository);
             RevTree revTree = revWalk.parseCommit(parent).getTree();
             TreeWalk treeWalk = new TreeWalk(repository);
@@ -603,18 +646,21 @@ public class ConflictManagementToolController {
     }
 
     private ArrayList<String> getBaseParentFiles(String fileName){
+        Main.assertFxThread();
         ObjectId baseParent = ObjectId.fromString(mergeResult.get("baseParent").substring(7,47));
-        //System.out.println(mergeResult.get("baseParent"));
-        //System.out.println(baseParent);
+        System.out.println(mergeResult.get("baseParent"));
+        System.out.println(baseParent);
         return getParentFiles(baseParent, fileName);
     }
 
     private ArrayList<String> getMergedParentFiles(String fileName){
+        Main.assertFxThread();
         ObjectId mergedParent = ObjectId.fromString(mergeResult.get("mergedParent").substring(7,47));
         return getParentFiles(mergedParent, fileName);
     }
 
     private void getActualConflictingLines(ArrayList<ConflictLine> allConflictLines, ArrayList<ConflictLine> conflictLines, ArrayList<Integer> conflictingLineNumbers) {
+        Main.assertFxThread();
         int lineNumber = 0;
         for (ConflictLine conflictLine : allConflictLines) {
             if (conflictLine.isConflicting()) {
@@ -627,11 +673,13 @@ public class ConflictManagementToolController {
     }
 
     private void setInitialPositions(CodeArea doc, ArrayList<Integer> conflictingLineNumbers) {
+        Main.assertFxThread();
         doc.moveTo(conflictingLineNumbers.get(0), 0);
         doc.requestFollowCaret();
     }
 
     private void bindMouseMovementToConflict(CodeArea doc, ArrayList<Integer> conflictingLineNumbers, ArrayList<ConflictLine> conflictLines) {
+        Main.assertFxThread();
         doc.setOnMouseClicked(e -> {
             int currentLine = doc.getCurrentParagraph();
 
@@ -655,6 +703,7 @@ public class ConflictManagementToolController {
     }
 
     private void setModifyingButtonsDisabled(boolean disabled) {
+        Main.assertFxThread();
         leftUndo.setDisable(disabled);
         leftReject.setDisable(disabled);
         leftAccept.setDisable(disabled);
@@ -665,6 +714,7 @@ public class ConflictManagementToolController {
     }
 
     private CodeArea setLines(ArrayList<ConflictLine> lines, CodeArea doc) {
+        Main.assertFxThread();
         for (ConflictLine conflict : lines) {
             ArrayList<String> conflictLines = conflict.getLines();
             for (String line : conflictLines) {
@@ -681,27 +731,32 @@ public class ConflictManagementToolController {
     }
 
     private void setCSSSelector(CodeArea doc, int startIndex, int endIndex, String selector) {
+        Main.assertFxThread();
         doc.setStyle(startIndex, endIndex, Collections.singleton(selector));
     }
 
     private void showAllConflictsHandledNotification() {
+        Main.assertFxThread();
         logger.info("All conflicts were handled.");
         notificationPaneController.addNotification("All conflicts were handled. Click apply to use them.");
     }
 
     // TODO: add a button or something that allows them to continue
     private void showNotAllConflictHandledNotification() {
+        Main.assertFxThread();
         logger.info("Apply clicked before finishing merge.");
         notificationPaneController.addNotification("Not all conflicts have been handled. Are you sure you want to continue?");
     }
 
     private void showAttemptingToAcceptANonConflictNotification() {
+        Main.assertFxThread();
         logger.info("Accept conflict clicked when there is not a conflict to add (either not a conflict or already handled).");
         notificationPaneController.addNotification("You are either trying to integrate something that is not "
                 + "conflicting \n or you already handled. Click the undo button if you made a mistake");
     }
 
     private void showAttemptingToRejectANonConflictNotification() {
+        Main.assertFxThread();
         logger.info("Reject conflict clicked when there is not a conflict to reject (either not a conflict or already handled).");
         notificationPaneController.addNotification("You are either trying to ignore something that is not "
                 + "conflicting \n or you already handled. Click the undo button if you made a mistake");
